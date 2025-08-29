c2ad4f82b90477501aeb3b6612ecbac90864b9f2
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-29 17:16:51 +0900

    ✨ Feature: button size variant (#9)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,26 +2,29 @@ import type { ComponentProps } from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/utils'
 
-const buttonVariants = cva('rounded-lg disabled:opacity-50', {
-  variants: {
-    variant: {
-      primary: '',
-      secondary: '',
-      outline: '',
-      ghost: '',
-      danger: '',
+const buttonVariants = cva(
+  'font-medium text-base leading-5 rounded-lg disabled:opacity-50 text-center',
+  {
+    variants: {
+      variant: {
+        primary: '',
+        secondary: '',
+        outline: '',
+        ghost: '',
+        danger: '',
+      },
+      size: {
+        sm: 'rounded-[0.375rem] px-3 py-2', //radius 6px
+        md: 'px-4 py-2.5',
+        lg: 'leading-6 px-6 py-3',
+      },
     },
-    size: {
-      sm: '',
-      md: '',
-      lg: '',
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
     },
-  },
-  defaultVariants: {
-    variant: 'primary',
-    size: 'md',
-  },
-})
+  }
+)
 
 interface ButtonProps
   extends ComponentProps<'button'>,
