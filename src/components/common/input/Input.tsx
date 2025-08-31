7fb9f99051ecf46d13d08844e344f759f6392304
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-31 22:33:38 +0900

    ✨ Feature: 검색 아이콘 & 이메일 아이콘 추가 (#23)

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,3 +1,5 @@
+import { Mail, Search } from 'lucide-react'
+
 function Input() {
   return (
     <label className="flex flex-col gap-2">
@@ -6,11 +8,15 @@ function Input() {
         <span className="text-danger-500">*</span>
       </div>
 
-      <input
-        type="text"
-        placeholder="이름을 입력하세요"
-        className="focus:ring-primary-500 rounded-lg p-1 px-[17px] py-[13px] text-sm ring ring-gray-300 outline-none placeholder:text-gray-400 focus:border-none focus:ring-2"
-      />
+      <div className="relative">
+        <Search className="absolute inset-y-0 left-2 my-auto h-4 text-gray-400" />
+        <Mail className="absolute inset-y-0 left-2 my-auto h-4 text-gray-400" />
+        <input
+          type="text"
+          placeholder="이름을 입력하세요"
+          className="focus:ring-primary-500 rounded-lg p-1 px-[17px] py-[13px] text-sm ring ring-gray-300 outline-none placeholder:text-gray-400 focus:border-none focus:ring-2"
+        />
+      </div>
     </label>
   )
 }
