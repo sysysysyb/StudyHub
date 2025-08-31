fa500a887b2b92b9ab7a4b3009dab7c2e3744e25
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-31 23:29:38 +0900

    ✨ Feature: isError 유무에 따라 input 스타일 변화 (#23)

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -5,6 +5,11 @@ const inputStyle = cva(
   'w-full focus:ring-primary-500 rounded-lg py-[13px] text-sm ring ring-gray-300 outline-none placeholder:text-gray-400 focus:border-none focus:ring-2',
   {
     variants: {
+      isError: {
+        true: 'ring-[#fca5a5]',
+        false:
+          'ring-gray-300 focus:ring-primary-500 focus:border-none focus:ring-2',
+      },
       hasIcon: {
         true: 'pl-10 pr-[17px]',
         false: 'px-[17px]',
