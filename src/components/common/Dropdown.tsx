0287fb5b904b17539654e7659a7132018e80519b
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-01 14:48:44 +0900

    ✨ Feature: 드롭다운 옵션 요소를 컴포넌트로 분리 (#24)

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,6 +1,12 @@
 import { ChevronDown } from 'lucide-react'
 import Input from './Input'
 
+function DropdownItem({ option }) {
+  return (
+    <div className="cursor-pointer px-4 py-3 hover:bg-gray-100">{option}</div>
+  )
+}
+
 function Dropdown() {
   return (
     <div className="flex flex-col gap-1">
@@ -8,12 +14,7 @@ function Dropdown() {
         <Input className="placeholder:text-gray-500 focus:ring-1 focus:ring-gray-300" />
         <ChevronDown className="absolute inset-y-0 right-2 my-auto h-[14px]" />
       </div>
-      <div className="h-36 overflow-auto rounded-lg text-base font-normal ring-1 ring-gray-300">
-        <div className="cursor-pointer px-4 py-3 hover:bg-gray-100">옵션1</div>
-        <div className="cursor-pointer px-4 py-3 hover:bg-gray-100">옵션2</div>
-        <div className="cursor-pointer px-4 py-3 hover:bg-gray-100">옵션3</div>
-        <div className="cursor-pointer px-4 py-3 hover:bg-gray-100">옵션4</div>
-      </div>
+      <div className="h-36 overflow-auto rounded-lg text-base font-normal ring-1 ring-gray-300"></div>
     </div>
   )
 }
