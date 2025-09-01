ab5297ffa2347c743fdc47c967bde634a509c0d9
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-01 20:23:51 +0900

    ✨ Feature: 컴포넌트 베럴 파일에 Input 추가 (#57)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@
 import Badge from '@/components/common/Badge'
 import Button from '@/components/common/Button'
 import ImageCard from '@/components/common/Card/ImageCard'
+import Input from '@/components/common/Input'
 
-export { Badge, Button, ImageCard }
+export { Badge, Button, ImageCard, Input }
