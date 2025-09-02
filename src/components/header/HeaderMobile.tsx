d88cd63e02e5cbb074b2472f1b7eccd68c9a3393
dirage1 <dirage4635@gmail.com>
2025-09-02 15:05:26 +0900

    ✨ Feature: 모바일 헤더 생성 (#62)

--- src/components/header/HeaderMobile.tsx
+++ src/components/header/HeaderMobile.tsx
@@ -1,13 +1,14 @@
 import type { ReactNode } from 'react'
 import { Symbol } from '../common/Symbol'
+import { AlignJustify } from 'lucide-react'
 
 export function MobileHeader(): ReactNode {
   return (
-    <div className="flex justify-start px-4">
-      <div>
-        <button></button>
-        <Symbol />
-      </div>
+    <div className="flex items-center justify-start gap-4 px-6">
+      <button className="cursor-pointer">
+        <AlignJustify />
+      </button>
+      <Symbol />
     </div>
   )
 }
