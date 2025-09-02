a2609e04abe4d7e7316bd67c1c68aa5cb844b463
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-02 16:28:26 +0900

    ♻️ Refactor: 토스트 컨테이너 수정 (#25)
    토스트 아이템에 id 값 전달 / UX 향상을 위해 debounce 딜레이 0.2초로 수정

--- src/components/common/Toast/ToastContainer.tsx
+++ src/components/common/Toast/ToastContainer.tsx
@@ -7,7 +7,7 @@ import type { Toast } from '@/types'
 function ToastContainer() {
   const [renderedToasts, setRenderedToasts] = useState<Toast[]>([])
   const { toasts } = useToastStore()
-  const debouncedToasts = useDebounce(toasts, 500)
+  const debouncedToasts = useDebounce(toasts, 200)
 
   useEffect(() => {
     setRenderedToasts(debouncedToasts)
@@ -16,7 +16,7 @@ function ToastContainer() {
   return (
     <div className="fixed top-18 right-1 z-10 w-112 space-y-1">
       {renderedToasts.map(({ id, type, content }) => (
-        <ToastItem key={id} type={type} content={content} />
+        <ToastItem key={id} id={id} type={type} content={content} />
       ))}
     </div>
   )
