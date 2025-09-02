d38e30f57a04048b570500a7691464780a51a4db
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-02 14:37:12 +0900

    ✨ Feature: 항상 뷰포트의 특정 위치에 렌더링되도록 main.tsx에 추가 (#25)

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { createRoot } from 'react-dom/client'
 
 import '@/index.css'
 import App from '@/App.tsx'
+import ToastContainer from '@/components/common/Toast/ToastContainer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ToastContainer />
   </StrictMode>
 )
