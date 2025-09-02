aaafd5bc79bc1eccb32bd65cb7a3ce4756e14c3f
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-02 14:31:45 +0900

    ✨ Feature: Chat modal UI (#63)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,6 +4,6 @@ import Button from '@/components/common/Button'
 import ImageCard from '@/components/common/Card/ImageCard'
 import Input from '@/components/common/Input'
 import MyPageSideBar from '@/components/my-page-side-bar/MyPageSideBar'
+import Chat from '@/components/chat/chat'
 
-export { Badge, Button, ImageCard, Input, MyPageSideBar }
-
+export { Badge, Button, ImageCard, Input, MyPageSideBar, Chat }
