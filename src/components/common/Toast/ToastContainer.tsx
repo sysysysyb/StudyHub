a76172f45f629d93c0662f0af577d777ead4fbbb
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-02 14:34:38 +0900

    ✨ Feature: 토스트 컨테이너에 전역 변수 토스트 목록 가져와 map 실행 (#25)

--- src/components/common/Toast/ToastContainer.tsx
+++ src/components/common/Toast/ToastContainer.tsx
@@ -1,11 +1,14 @@
+import { useToastStore } from '@/store/useToastStore'
 import ToastItem from './ToastItem'
 
 function ToastContainer() {
+  const { toasts } = useToastStore()
+
   return (
     <div className="fixed top-18 right-2 z-10 space-y-1">
-      <ToastItem type="success" />
-      <ToastItem type="warning" />
-      <ToastItem type="error" />
+      {toasts.map(({ id, type, content }) => (
+        <ToastItem key={id} type={type} content={content} />
+      ))}
     </div>
   )
 }
