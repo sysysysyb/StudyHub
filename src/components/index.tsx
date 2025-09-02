ba857588378d87bbab9414affe3a5383520218c3
dirage1 <dirage4635@gmail.com>
2025-09-02 13:15:26 +0900

    🚚 Rename: Header.tsx 이름 변경, 디렉토리 이동, 호출방식 변경, 호출 수정 (#59)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,6 +4,6 @@ import Button from '@/components/common/Button'
 import ImageCard from '@/components/common/Card/ImageCard'
 import Input from '@/components/common/Input'
 import MyPageSideBar from '@/components/my-page-side-bar/MyPageSideBar'
+import Header from './header/Header'
 
-export { Badge, Button, ImageCard, Input, MyPageSideBar }
-
+export { Badge, Button, ImageCard, Input, MyPageSideBar, Header }
