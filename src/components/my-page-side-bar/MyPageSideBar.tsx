380543f9c552a06da89517636a0d8211e9b6f707
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-02 11:38:58 +0900

    ✨ Feature: render MyPageSidebarMenu (#47)

--- src/components/my-page-side-bar/MyPageSideBar.tsx
+++ src/components/my-page-side-bar/MyPageSideBar.tsx
@@ -1,10 +1,57 @@
 import { Card } from '@/components/common/Card/Card'
 import MyPageSideBarProfile from './MyPageSideBarProfile'
+import MyPageSideBarMenu from './MyPageSideBarMenu'
+import type { ComponentProps } from 'react'
+import {
+  User as UserIcon,
+  Bookmark as BookmarkIcon,
+  Newspaper as NewspaperIcon,
+  Medal as MedalIcon,
+} from 'lucide-react'
+
+const MY_PAGE_SIDE_BAR_MENU_ITEMS: ComponentProps<typeof MyPageSideBarMenu>[] =
+  [
+    {
+      title: '내 정보',
+      description: '개인 정보 조회 및 수정',
+      icon: UserIcon,
+    },
+    {
+      title: '북마크한 공고',
+      description: '저장한 공고 목록',
+      icon: BookmarkIcon,
+    },
+    {
+      title: '북마크한 강의',
+      description: '저장한 강의 목록',
+      icon: BookmarkIcon,
+    },
+    {
+      title: '지원 내역',
+      description: '스터디 지원 현황',
+      icon: NewspaperIcon,
+    },
+    {
+      title: '완료한 스터디',
+      description: '수료한 스터디 목록',
+      icon: MedalIcon,
+    },
+  ]
 
 export default function MyPageSideBar() {
   return (
-    <Card className="flex flex-col items-center justify-start">
+    <Card className="flex w-[280px] flex-col items-center justify-start">
       <MyPageSideBarProfile />
+      <div className="mt-8 flex w-full flex-col items-center justify-start space-y-2">
+        {MY_PAGE_SIDE_BAR_MENU_ITEMS.map(({ title, description, icon }, i) => (
+          <MyPageSideBarMenu
+            title={title}
+            description={description}
+            icon={icon}
+            key={i}
+          />
+        ))}
+      </div>
     </Card>
   )
 }
