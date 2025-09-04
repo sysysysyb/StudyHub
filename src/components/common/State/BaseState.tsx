d0e40ad86ed04fa902a205891e4de1b95a6c7626
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-04 21:19:42 +0900

    ✨ Feature: 화면 상태 UI의 Base UI 컴포넌트 추가 (#80)

---
+++ src/components/common/State/BaseState.tsx
@@ -0,0 +1,60 @@
+import { type LucideIcon } from 'lucide-react'
+import Button from '@/components/common/Button'
+import { cn } from '@/utils'
+import type { ReactNode } from 'react'
+
+interface BaseStateProps {
+  icon?: LucideIcon
+  iconColor?: string
+  iconBg?: string
+  title: string
+  description: string
+  buttonValue?: ReactNode
+  buttonType?: 'primary' | 'outline'
+  buttonClassName?: string
+  onClick?: () => void
+}
+
+function BaseState({
+  icon: Icon,
+  iconColor,
+  iconBg,
+  title,
+  description,
+  buttonValue,
+  buttonType,
+  buttonClassName,
+  onClick,
+}: BaseStateProps) {
+  return (
+    <div className="flex size-full flex-col items-center justify-center gap-6 rounded-lg border border-solid border-gray-200 bg-gray-50">
+      {Icon ? (
+        <div
+          className={cn(
+            'flex size-20 items-center justify-center rounded-full',
+            iconBg
+          )}
+        >
+          <Icon className={cn('size-[30px]', iconColor)} />
+        </div>
+      ) : (
+        <div className="border-primary-500 animate-loading-state size-12 rounded-full border-b-2 border-solid" />
+      )}
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h4 className="text-xl font-semibold text-gray-900">{title}</h4>
+        <p className="text-base/6 font-normal text-gray-500">{description}</p>
+      </div>
+      {buttonValue && (
+        <Button
+          variant={buttonType}
+          className={buttonClassName}
+          onClick={onClick}
+        >
+          {buttonValue}
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export default BaseState
