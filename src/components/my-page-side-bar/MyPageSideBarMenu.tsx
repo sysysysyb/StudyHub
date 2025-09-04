2bf2f4a01af062f7e6b777160a50e322b351f02f
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-04 16:48:56 +0900

    ✨ Feature: MyPageSideBarMenu url이 같으면 색 변경 (#76)

--- src/components/my-page-side-bar/MyPageSideBarMenu.tsx
+++ src/components/my-page-side-bar/MyPageSideBarMenu.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardTitle } from '@/components/common/Card/Card'
+import { cn } from '@/utils'
 import type { LucideIcon } from 'lucide-react'
-import { Link } from 'react-router'
+import { Link, useLocation } from 'react-router'
 
 interface MyPageSideBarMenuProps {
   title: string
@@ -15,11 +16,18 @@ export default function MyPageSideBarMenu({
   icon: Icon,
   url,
 }: MyPageSideBarMenuProps) {
+  const location = useLocation()
+
   return (
     <Link to={url} className="w-full">
       <Card
         variant={'flat'}
-        className="hover:text-primary-800 hover:bg-primary-100 flex w-full cursor-pointer items-center justify-start space-x-3 px-4 py-3 text-gray-700 transition-colors"
+        className={cn(
+          'hover:text-primary-800 hover:bg-primary-100 flex w-full cursor-pointer items-center justify-start space-x-3 px-4 py-3 transition-colors',
+          location.pathname === url
+            ? 'text-primary-800 bg-primary-100'
+            : 'text-gray-700'
+        )}
       >
         <Icon />
         <div className="flex flex-col">
