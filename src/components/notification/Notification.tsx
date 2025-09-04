adf3b1570877d0242b49dee766100543fd2270d8
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-04 18:23:27 +0900

    ♻️ Refactor: 알림 창을 카드 기반에서 모달 기반으로 변경 (#78)

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,20 +1,29 @@
 import { Bell as BellIcon } from 'lucide-react'
-import { useState } from 'react'
-import { Card } from '@/components/common/Card/Card'
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalTitle,
+  ModalTrigger,
+} from '@/components/common/Modal'
 
 export default function Notification() {
-  const [isShow, setIsShow] = useState(false)
-
   return (
     <div className="relative">
-      <button
-        onClick={() => {
-          setIsShow(!isShow)
-        }}
-      >
-        <BellIcon />
-      </button>
-      {isShow ? <Card>알림</Card> : null}
+      <Modal isOverlay={false}>
+        <ModalTrigger>
+          <BellIcon className="h-5 text-gray-600" />
+        </ModalTrigger>
+        <ModalContent
+          isPositionCenter={false}
+          className="absolute top-10 right-0 h-96 w-80 overflow-hidden rounded-lg border border-gray-200 shadow"
+        >
+          <ModalHeader>
+            <ModalTitle>알림</ModalTitle>
+          </ModalHeader>
+          <ModalContent>알림 읽어!</ModalContent>
+        </ModalContent>
+      </Modal>
     </div>
   )
 }
