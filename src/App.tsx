3ecea2db62d8f94e757ae2899e100a2425c76415
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-04 14:32:53 +0900

    🔧 Feature: setting routing (#74)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { Header } from '@/components'
+import { Routes, Route } from 'react-router'
 import { LandingPage } from '@/pages'
 
 function App() {
   return (
-    <>
-      <Header />
-      <LandingPage />
-    </>
+    <Routes>
+      <Route>
+        <Route index element={<LandingPage />} />
+
+        <Route path="auth">
+          <Route path="login" element={<div>login</div>} />
+          <Route path="signup" element={<div>signup</div>} />
+          <Route path="find-email" element={<div>find-email</div>} />
+          <Route path="find-password" element={<div>find-password</div>} />
+          <Route path="restore-user" element={<div>restore-user</div>} />
+        </Route>
+
+        <Route path="my-page">
+          <Route index element={<div>my-page</div>} />
+          <Route
+            path="bookmarked-recruitment"
+            element={<div>bookmarked-recruitment</div>}
+          />
+          <Route
+            path="bookmarked-lecture"
+            element={<div>bookmarked-lecture</div>}
+          />
+          <Route path="applied-study" element={<div>applied-study</div>} />
+          <Route path="completed-study" element={<div>completed-study</div>} />
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
