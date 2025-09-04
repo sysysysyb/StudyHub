fa98fc24849b4e7d64fd344975970fabdd47215b
dirage1 <dirage4635@gmail.com>
2025-09-04 15:02:47 +0900

    🐛 Fix: 리베이스 컨플릭트 해결 (#68)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,17 +11,10 @@ export function Footer(): ReactNode {
           <div className="pb-4">
             <Logo />
           </div>
-<<<<<<< HEAD
           <p>
             IT 전문가로 성장하는 여정에 함께합니다. 최고의 강의와 스터디
             그룹으로 실무 역량을 키워보세요.
           </p>
-=======
-          <div>
-            IT 전문가로 성장하는 여정에 함께합니다. 최고의 강의와 스터디
-            그룹으로 실무 역량을 키워보세요.
-          </div>
->>>>>>> 052c3b7 (✨ Feature: 루트 레이아웃에 푸터 추가 (#68))
         </div>
         <FooterNavComponent
           category="서비스"
@@ -31,11 +24,6 @@ export function Footer(): ReactNode {
             { label: '구인 공고' },
           ]}
         />
-<<<<<<< HEAD
-=======
-
-        {/* 지원 */}
->>>>>>> 052c3b7 (✨ Feature: 루트 레이아웃에 푸터 추가 (#68))
         <FooterNavComponent
           category="지원"
           NavButtons={[
