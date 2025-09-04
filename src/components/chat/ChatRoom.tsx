061630af3bdea91c027ac14a5b76883b6708abf5
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-04 16:09:41 +0900

    ♻️ Refactor: split ChatRoomUsers component (#63)

--- src/components/chat/ChatRoom.tsx
+++ src/components/chat/ChatRoom.tsx
@@ -1,6 +1,7 @@
 import type { ChatRoomMessages } from '@/types/api-response-types/chat-response-types'
 import { cn } from '@/utils'
 import ChatRoomInput from './ChatRoomInput'
+import ChatRoomUsers from './ChatRoomUsers'
 
 const dummyChatRoomMessages: ChatRoomMessages = {
   next_cursor: 'cursor_2',
@@ -64,23 +65,7 @@ interface ChatRoomProps {
 export default function ChatRoom({ chatRoomId }: ChatRoomProps) {
   return (
     <div>
-      {/* TODO: 마우스 드래그로 스크롤 할 수 있게 변경 */}
-      <section className="sticky top-0 flex items-center justify-start space-x-2 overflow-x-scroll border-b border-gray-200 bg-gray-200 p-2">
-        {dummyChatRoomUsers.map(({ username, isOnline }, i) => (
-          <div
-            key={i}
-            className="flex items-center justify-center space-x-2 rounded-full bg-white px-2 py-1"
-          >
-            <div
-              className={cn(
-                'size-2 rounded-full',
-                isOnline ? 'bg-success-500' : 'bg-gray-300'
-              )}
-            />
-            <span className="whitespace-nowrap">{username}</span>
-          </div>
-        ))}
-      </section>
+      <ChatRoomUsers users={dummyChatRoomUsers} />
       <section className="flex flex-col space-y-3">
         {dummyChatRoomMessages.results.map((result) => {
           const {
