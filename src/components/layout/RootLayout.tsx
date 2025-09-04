48f70f8fe090ea8dfab51487c47876cd13c9d80a
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-04 15:03:22 +0900

    ✨ Feature: Chat 컴포넌트 RootLayout에 추가 (#63)

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components'
+import { Chat, Header } from '@/components'
 import { Outlet } from 'react-router'
 
 export default function RootLayout() {
@@ -6,6 +6,7 @@ export default function RootLayout() {
     <div>
       <Header />
       <Outlet />
+      <Chat />
     </div>
   )
 }
