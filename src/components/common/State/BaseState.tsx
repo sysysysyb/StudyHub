c4c156474cb09b24792b0e307ee7988022436c8f
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-04 21:31:56 +0900

    ♻️ Refactor: Icon과 Spinner 컴포넌트 분리 (#80)

--- src/components/common/State/BaseState.tsx
+++ src/components/common/State/BaseState.tsx
@@ -15,8 +15,27 @@ interface BaseStateProps {
   onClick?: () => void
 }
 
+function Icon({ icon: CurrentIcon, iconBg, iconColor }: BaseStateProps) {
+  return (
+    <div
+      className={cn(
+        'flex size-20 items-center justify-center rounded-full',
+        iconBg
+      )}
+    >
+      {CurrentIcon && <CurrentIcon className={cn('size-[30px]', iconColor)} />}
+    </div>
+  )
+}
+
+function Spinner() {
+  return (
+    <div className="border-primary-500 animate-loading-state size-12 rounded-full border-b-2 border-solid" />
+  )
+}
+
 function BaseState({
-  icon: Icon,
+  icon,
   iconColor,
   iconBg,
   title,
@@ -28,17 +47,10 @@ function BaseState({
 }: BaseStateProps) {
   return (
     <div className="flex size-full flex-col items-center justify-center gap-6 rounded-lg border border-solid border-gray-200 bg-gray-50">
-      {Icon ? (
-        <div
-          className={cn(
-            'flex size-20 items-center justify-center rounded-full',
-            iconBg
-          )}
-        >
-          <Icon className={cn('size-[30px]', iconColor)} />
-        </div>
+      {icon ? (
+        <Icon icon={icon} iconColor={iconColor} iconBg={iconBg} />
       ) : (
-        <div className="border-primary-500 animate-loading-state size-12 rounded-full border-b-2 border-solid" />
+        <Spinner />
       )}
       <div className="flex flex-col items-center justify-center gap-2">
         <h4 className="text-xl font-semibold text-gray-900">{title}</h4>
