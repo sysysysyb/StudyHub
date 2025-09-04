3d0d9e4bbddf3ab0604c16d2e4b8c3edef0810b2
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-04 15:25:36 +0900

    ✨ Feature: 채팅방 멤버 UI (#63)

--- src/components/chat/ChatRoom.tsx
+++ src/components/chat/ChatRoom.tsx
@@ -64,6 +64,22 @@ interface ChatRoomProps {
 export default function ChatRoom({ chatRoomId }: ChatRoomProps) {
   return (
     <div>
+      <section className="sticky top-0 flex items-center justify-start space-x-2 overflow-x-scroll border-b border-gray-200 bg-gray-200 p-2">
+        {dummyChatRoomUsers.map(({ username, isOnline }, i) => (
+          <div
+            key={i}
+            className="flex items-center justify-center space-x-2 rounded-full bg-white px-2 py-1"
+          >
+            <div
+              className={cn(
+                'size-2 rounded-full',
+                isOnline ? 'bg-success-500' : 'bg-gray-300'
+              )}
+            />
+            <span className="whitespace-nowrap">{username}</span>
+          </div>
+        ))}
+      </section>
       <section className="flex flex-col space-y-3">
         {dummyChatRoomMessages.results.map((result) => {
           const {
