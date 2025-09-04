7169bbb945e279ad6352cd96482d1d7e3c9dfc6e
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-05 00:16:05 +0900

    ♻️ Refactor: 로딩 상태 UI를 복합 컴포넌트 구조로 수정 (#80)

--- src/components/common/State/LoadingState.tsx
+++ src/components/common/State/LoadingState.tsx
@@ -1,12 +1,22 @@
-import BaseState from './BaseState'
+import type { ComponentProps } from 'react'
+import {
+  BaseStateContent,
+  BaseStateSpinner,
+  BaseStateWrapper,
+} from './BaseState'
 
-function LoadingState({ className }: React.HTMLAttributes<HTMLDivElement>) {
+function LoadingState({
+  className,
+  ...props
+}: ComponentProps<typeof BaseStateWrapper>) {
   return (
-    <BaseState
-      title="데이터를 불러오고 있습니다"
-      description="잠시만 기다려주세요..."
-      className={className}
-    />
+    <BaseStateWrapper className={className} {...props}>
+      <BaseStateSpinner />
+      <BaseStateContent
+        title="데이터를 불러오고 있습니다"
+        description="잠시만 기다려주세요..."
+      />
+    </BaseStateWrapper>
   )
 }
 
