50e3bb126c626335472cb233b82e190f72bf1845
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-05 21:57:09 +0900

    ✨ Feature: Logo 컴포넌트 수정 (#91)
    기존보다 큰 사이즈 추가 / primary와 black 두 가지 색상 추가

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,5 +1,23 @@
-export const Logo = () => {
-  return (
-    <div className="text-primary-600 text-heading4 font-bold">StudyHub</div>
-  )
+import { cn } from '@/utils'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+const logoVariants = cva('font-bold', {
+  variants: {
+    size: {
+      md: 'text-heading4',
+      lg: 'text-[2rem]',
+    },
+    color: {
+      primary: 'text-primary-600',
+      black: 'text-black',
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+    color: 'primary',
+  },
+})
+
+export const Logo = ({ size, color }: VariantProps<typeof logoVariants>) => {
+  return <div className={cn(logoVariants({ size, color }))}>StudyHub</div>
 }
