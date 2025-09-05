30fd46a2c3152abfbcd025b3e3466f79e1ffb3de
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-05 10:52:39 +0900

    🏷️ Types: notificationType 타입 추가 (#78)

--- src/types/api-response-types/notification-response-types.ts
+++ src/types/api-response-types/notification-response-types.ts
@@ -7,10 +7,18 @@ export interface NotificationList {
   sort: string
 }
 
+export type notificationType =
+  | 'STUDY_JOIN'
+  | 'STUDY_EXIT'
+  | 'APPLICATION_ACCEPT'
+  | 'APPLICATION_REJECT'
+  | 'ADD_APPLICATION'
+  | 'NEW_CHAT_MESSAGE'
+
 export interface Notification {
   notification_id: number
   content: string
-  type: string
+  type: notificationType
   is_read: boolean
   redirect_url: string
   created_at: Date
