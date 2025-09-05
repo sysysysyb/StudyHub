7c185364cb894de50e8e074c982d1b1fa1bcb0e0
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-05 17:15:06 +0900

    💄 Design: 알림 카드 디자인 (#78)

--- src/components/notification/NotificationCard.tsx
+++ src/components/notification/NotificationCard.tsx
@@ -1,6 +1,7 @@
 import type { Notification } from '@/types/api-response-types/notification-response-types'
 import { cn } from '@/utils'
 import { Link } from 'react-router'
+import NotificationIcon from '@/components/notification/NotificationIcon'
 
 interface NotificationCardProps {
   notification: Notification
@@ -24,14 +25,14 @@ export default function NotificationCard({
     <Link
       to="#"
       className={cn(
-        'flex w-full items-center justify-between',
-        isRead ? 'bg-white' : 'bg-primary-100'
+        'flex w-full items-center justify-start gap-3 p-4',
+        isRead ? 'bg-white' : 'bg-primary-50'
       )}
     >
-      <div>icon</div>
-      <div className="flex flex-col items-center justify-center">
-        <span className="text">{content}</span>
-        <span>{`${createdMonth}월 ${createdDate}일`}</span>
+      <NotificationIcon notificationType={type} />
+      <div className="flex flex-1 flex-col items-start justify-center">
+        <span className="text-gray-900">{content}</span>
+        <span className="text-xs text-gray-500">{`${createdMonth}월 ${createdDate}일`}</span>
       </div>
       {isRead || <div className="bg-primary-500 size-2 rounded-full" />}
     </Link>
