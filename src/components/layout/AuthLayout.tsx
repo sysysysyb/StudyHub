3492ceb0951f26c27715f325727b33a733c434d6
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-05 12:10:44 +0900

    ♻️ Refactor: export default 적용 (#87)

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router'
 
-export function AuthLayout() {
+export default function AuthLayout() {
   return (
     <div className="flex items-center justify-center py-0 sm:py-30">
       <Outlet />
