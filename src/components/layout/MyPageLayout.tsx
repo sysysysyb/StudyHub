1cd9f0c5522681f95867ca9a9ab9a44036ba37f6
dirage1 <dirage4635@gmail.com>
2025-09-05 17:36:06 +0900

    💄 Design: 마이페이지 레이아웃에 스타일 추가지정 (#88)

--- src/components/layout/MyPageLayout.tsx
+++ src/components/layout/MyPageLayout.tsx
@@ -3,9 +3,9 @@ import { MyPageSideBar } from '@/components'
 
 export default function MyPageLayout() {
   return (
-    <div className="flex items-start justify-start gap-8 bg-gray-100 p-8">
+    <div className="flex items-start justify-center gap-8 bg-gray-100 p-8">
       <MyPageSideBar />
-      <div className="min-h-[700px] flex-1 rounded-lg border border-gray-200 bg-white p-6">
+      <div className="min-h-[700px] max-w-4xl flex-1 rounded-lg border border-gray-200 bg-white p-8">
         <Outlet />
       </div>
     </div>
