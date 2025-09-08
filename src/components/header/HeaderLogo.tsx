09dadfcba7aa9b1ee54827cdaa8ebead518d1b78
dirage1 <dirage4635@gmail.com>
2025-09-08 13:25:06 +0900

    💄 Style: 헤더로고 링크 루트 연동으로 수정 (#107)

--- src/components/header/HeaderLogo.tsx
+++ src/components/header/HeaderLogo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 
 export const HeaderLogo = () => {
   return (
-    <Link to="/main" className="flex h-12 w-36 items-center gap-2">
+    <Link to="/" className="flex h-12 w-36 items-center gap-2">
       <Symbol />
       <Logo />
     </Link>
