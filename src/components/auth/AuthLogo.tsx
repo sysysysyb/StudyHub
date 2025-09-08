a4db81081441ea7c13a977834168ac01e41f216a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-08 11:36:17 +0900

    ✨ Feature: 로고 lg 사이즈 추가 및 스타일 수정 (#90)

--- src/components/auth/AuthLogo.tsx
+++ src/components/auth/AuthLogo.tsx
@@ -4,11 +4,14 @@ import { Logo } from '../common/Logo'
 
 export function AuthLogo({ className, ...props }: ComponentProps<'div'>) {
   return (
-    <div className={cn('flex justify-center gap-2', className)} {...props}>
+    <div
+      className={cn('flex items-center justify-center gap-2', className)}
+      {...props}
+    >
       <div className="bg-primary-500 flex size-8 items-center justify-center rounded-lg text-base font-bold text-white">
         S
       </div>
-      <Logo />
+      <Logo size="lg" />
     </div>
   )
 }
