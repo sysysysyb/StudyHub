a77e0886ce082df38f64567ecd83f20a0aa33f9b
dirage1 <dirage4635@gmail.com>
2025-09-08 10:54:30 +0900

    💄 Design: 아바타 사이즈 상수 추가, 대체 아이콘 변경 (#93)

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -2,6 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import type { ComponentProps } from 'react'
 import { cn } from '@/utils'
 import { AVATAR_SIZE } from '@/components/avatar/AvatarSize'
+import { User as UserIcon } from 'lucide-react'
 
 const AvatarImage = cva(
   'text-primary-600 bg-primary-100 rounded-full relative shrink-0 flex items-center justify-center border-primary-200 border',
@@ -24,6 +25,7 @@ const AvatarState = cva(
         away: 'bg-primary-500',
         dnd: 'bg-danger-500',
         offline: 'bg-gray-400',
+        none: 'display:none',
       },
     },
     defaultVariants: {
@@ -55,8 +57,8 @@ export default function Avatar({
           className="h-full w-full rounded-full object-cover"
         />
       ) : (
-        <span>{alt?.charAt(0) || '김'}</span>
-        // 첫번째 글자만 가져와서 혹은 '김'으로 이미지 대체
+        <span>{alt?.charAt(0) || <UserIcon />}</span>
+        // 마이페이지 사이드바에서 쓰인 아이콘으로 대체.
       )}
       <span aria-hidden="true" className={cn(AvatarState({ state }))} />
     </div>
