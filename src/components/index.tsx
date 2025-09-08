a0c543b8e393d57f40d7e2f12c49100890726bc2
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-08 11:59:42 +0900

    ♻️ Refactor: Auth 전용 베럴 파일 분리 (#90)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -19,15 +19,6 @@ import Avatar from '@/components/avatar/Avatar'
 import AuthLayout from '@/components/layout/AuthLayout'
 import Notification from '@/components/notification/Notification'
 import { UserInfoDescription } from '@/components/mypage/InfoDescription'
-import { AuthContainer } from '@/components/auth/AuthContainer'
-import { AuthLogo } from '@/components/auth/AuthLogo'
-import { AuthTitle } from '@/components/auth/AuthTitle'
-import { AuthDescription } from '@/components/auth/AuthDescription'
-import { AuthLink } from '@/components/auth/AuthLink'
-import { AuthSubmitButton } from '@/components/auth/AuthSubmitButton'
-import { AuthVerifyButton } from '@/components/auth/AuthVerifyButton'
-import { AuthSocialLoginButton } from '@/components/auth/AuthSocialLoginButton'
-import { AuthBadge } from '@/components/auth/AuthBadge'
 
 export {
   Badge,
@@ -50,13 +41,4 @@ export {
   AuthLayout,
   Notification,
   UserInfoDescription,
-  AuthContainer,
-  AuthLogo,
-  AuthTitle,
-  AuthDescription,
-  AuthLink,
-  AuthSubmitButton,
-  AuthVerifyButton,
-  AuthSocialLoginButton,
-  AuthBadge,
 }
