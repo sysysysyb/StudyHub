9e44579e3cf19f6d4338e6b2094925e1225bd26c
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-08 17:38:13 +0900

    ✨ Feature: 헤더 로그인 메뉴 클릭 시 로그인 페이지로 이동 (#81)

--- src/components/header/NavList.tsx
+++ src/components/header/NavList.tsx
@@ -1,8 +1,11 @@
 import type { ReactNode } from 'react'
 import { NavComponent } from './NavComponent'
 import { Button } from '@/components'
+import { useNavigate } from 'react-router'
 
 export function HeaderNavList(): ReactNode {
+  const navigate = useNavigate()
+
   return (
     // 로그인 여부에 따른 조건부 렌더링을 위해 별도 컴포넌트화
     <div className="flex items-center">
@@ -10,7 +13,7 @@ export function HeaderNavList(): ReactNode {
       <NavComponent label="스터디 그룹" />
       <NavComponent label="구인 공고" />
       {/* 이 줄 이후의 요소들만 로그인 시 변경 예정 */}
-      <NavComponent label="로그인" />
+      <NavComponent label="로그인" event={() => navigate('/auth/login')} />
       <Button size="sm" className="ml-4 w-full">
         회원가입
       </Button>
