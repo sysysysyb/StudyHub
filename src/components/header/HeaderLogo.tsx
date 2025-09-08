325796d8544daf0eccbffbcc7e529cbcb2db43d1
dirage1 <dirage4635@gmail.com>
2025-09-08 13:19:04 +0900

    ♻️ Refactor: 헤더로고 홈버튼으로 변경 (#107)

--- src/components/header/HeaderLogo.tsx
+++ src/components/header/HeaderLogo.tsx
@@ -1,10 +1,11 @@
 import { Symbol, Logo } from '@/components'
+import { Link } from 'react-router'
 
 export const HeaderLogo = () => {
   return (
-    <div className="flex h-12 w-36 items-center gap-2">
+    <Link to="/main" className="flex h-12 w-36 items-center gap-2">
       <Symbol />
       <Logo />
-    </div>
+    </Link>
   )
 }
