fc595ca3d0ece03bfbd076e1b5757af4b9e50177
dirage1 <dirage4635@gmail.com>
2025-09-08 13:06:36 +0900

    💄 Design: 헤더 로고 간격 추가 (#107)

--- src/components/header/HeaderLogo.tsx
+++ src/components/header/HeaderLogo.tsx
@@ -2,7 +2,7 @@ import { Symbol, Logo } from '@/components'
 
 export const HeaderLogo = () => {
   return (
-    <div className="flex h-12 w-36 items-center">
+    <div className="flex h-12 w-36 items-center gap-2">
       <Symbol />
       <Logo />
     </div>
