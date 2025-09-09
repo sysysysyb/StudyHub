f8ec5bb2c560b9d34f3b2db11afcd94223c19b83
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-09 11:09:53 +0900

    🐛 Fix: msw path parameter 재설정 (#118)

--- src/mocks/handlers/chat.ts
+++ src/mocks/handlers/chat.ts
@@ -2,7 +2,7 @@ import { API_BASE_URL } from '@/constants/api-constants'
 import { http, HttpResponse } from 'msw'
 import { chatRoomMessages } from '@/mocks/data/chat-room-messages'
 
-const getChatMessages = http.get(
+const getChatMessages = http.get<{ study_group_uuid: string }>(
   `${API_BASE_URL}/chat/rooms/:study_group_uuid/messages`,
   () => {
     return HttpResponse.json(chatRoomMessages)
