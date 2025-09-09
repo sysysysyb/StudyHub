6d15aaed7f442d533f0ee8715a50ef700558c5b3
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-09 15:48:46 +0900

    🩹 Fix: 메세지 도착 시 자동으로 스크롤 내림 (#118)

--- src/tests/WebsocketTest.tsx
+++ src/tests/WebsocketTest.tsx
@@ -10,6 +10,7 @@ export default function WebsocketTest() {
 
   const webSocket = useRef<WebSocket | null>(null)
   const chatRoomId = 'chat-1111'
+  const divRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
     webSocket.current = new WebSocket(`ws://example/ws/chat/`)
@@ -33,11 +34,20 @@ export default function WebsocketTest() {
     }
   }, [chatRoomId])
 
+  useEffect(() => {
+    if (divRef.current) {
+      divRef.current.scrollTop = divRef.current.scrollHeight
+    }
+  }, [messages])
+
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-heading2">웹소켓 연결 테스트</h1>
       <span>5초에 한 번 씩 채팅이 도착해야합니다.</span>
-      <div className="flex h-[750px] w-full max-w-xl flex-col items-center justify-start gap-3 overflow-y-scroll rounded-2xl border border-gray-700 p-10">
+      <div
+        ref={divRef}
+        className="flex h-[750px] w-full max-w-xl flex-col items-center justify-start gap-3 overflow-y-scroll rounded-2xl border border-gray-700 p-10"
+      >
         {messages.map((message, i) => (
           <Card key={i} className="w-full">
             <ul className="w-full">
