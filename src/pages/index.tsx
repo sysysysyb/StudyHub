a110993c5e1d647dd3e7d15a5195cf4c1a64c52c
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-10 19:17:39 +0900

    ✨ Feature: pages 폴더 배럴 파일에 Signup 추가 (#82)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,6 @@ import LandingPage from '@/pages/LandingPage'
 import { MyInfo } from '@/pages/my-page/MyInfo'
 import BookmarkedRecruitment from '@/pages/my-page/BookmarkedRecruitment'
 import Login from '@/pages/Login'
+import Signup from '@/pages/Signup'
 
-export { LandingPage, MyInfo, BookmarkedRecruitment, Login }
+export { LandingPage, MyInfo, BookmarkedRecruitment, Login, Signup }
