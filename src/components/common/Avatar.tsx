06a8fe2de4cecc4eda3d83ba5ddb09777981a927
dirage1 <dirage4635@gmail.com>
2025-09-10 16:49:00 +0900

    🚚 상수파일명 스타일 통일을 위해 avatar-sizes로 파일명 수정 (#125)

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 import type { ComponentProps } from 'react'
 import { cn } from '@/utils'
-import { AVATAR_SIZE } from '@/constants/avatar-size'
+import { AVATAR_SIZE } from '@/constants/avatar-sizes'
 import { User as UserIcon } from 'lucide-react'
 
 const AvatarImage = cva(
