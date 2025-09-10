e6080e95e09a181bb7ffdfd3242fc9c69ee6a520
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-10 18:01:57 +0900

    💄 Design: Figma 디자인에 맞게 AuthBadge 스타일 수정 (#141)

--- src/components/auth/AuthBadge.tsx
+++ src/components/auth/AuthBadge.tsx
@@ -16,10 +16,10 @@ export function AuthBadge({
     <Badge
       size="lg"
       className={cn(
-        'cursor-pointer border border-solid px-8 py-2',
+        'cursor-pointer border border-solid px-8 py-2 font-semibold',
         isSelected
           ? 'bg-primary-100 border-primary-600 text-primary-600'
-          : 'border-[#cecece] bg-gray-200',
+          : 'border-[#cecece] bg-[#ececec] text-[#4d4d4d]',
         className
       )}
       {...props}
