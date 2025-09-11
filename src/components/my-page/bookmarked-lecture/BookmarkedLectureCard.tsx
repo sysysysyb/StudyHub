9edc6374ed567827c5908eae74ad42268e0ddc70
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-11 10:49:30 +0900

    ♻️ Refactor: split BookmarkedLectureCard component (#132)

---
+++ src/components/my-page/bookmarked-lecture/BookmarkedLectureCard.tsx
@@ -0,0 +1,55 @@
+import Button from '@/components/common/Button'
+import type { Lecture } from '@/types/api-response-types/lecture-response-type'
+import { BookmarkIcon } from 'lucide-react'
+import { Link } from 'react-router'
+
+interface BookmarkedLectureCardProps {
+  lecture: Lecture
+}
+
+export default function BookmarkedLectureCard({
+  lecture,
+}: BookmarkedLectureCardProps) {
+  const {
+    thumbnail_img_url: imageUrl,
+    title,
+    instructor,
+    difficulty,
+    platform,
+    original_price: originalPrice,
+    discount_price: discountPrice,
+    url_link: urlLink,
+    duration_hhmm: durationHHMM,
+  } = lecture
+
+  return (
+    <div className="flex items-center justify-center gap-6 rounded-xl border border-gray-200 p-6">
+      <img src={imageUrl} className="w-full max-w-40 rounded-lg" />
+      <div className="flex flex-1 items-center justify-between">
+        <div className="flex flex-1 flex-col">
+          <span className="text-heading5 text-gray-900">{title}</span>
+          <span className="text-gray-600">{instructor}</span>
+          <div className="flex items-center gap-3">
+            <span>{platform}</span>
+            <span>{difficulty}</span>
+            <span>{durationHHMM}</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-end justify-center gap-3">
+          <div className="flex flex-col items-end justify-start">
+            <span className="text-lg font-bold text-gray-900">{`₩${(originalPrice - discountPrice).toLocaleString()}`}</span>
+            {discountPrice > 0 ? (
+              <span className="text-sm text-gray-500 line-through">{`₩${originalPrice.toLocaleString()}`}</span>
+            ) : null}
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <BookmarkIcon className="text-primary-500 h-4" fill="#eab308" />
+            <Link to={urlLink}>
+              <Button className="text-sm">공고 보기</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/my-page/BookmarkedLecture.tsx
+++ src/pages/my-page/BookmarkedLecture.tsx
@@ -1,16 +1,14 @@
-import { Button, ListItemSkeleton } from '@/components'
+import { ListItemSkeleton } from '@/components'
 import { Input } from '@/components/common/input'
 import EmptyDataState from '@/components/common/State/EmptyDataState'
+import BookmarkedLectureCard from '@/components/my-page/bookmarked-lecture/BookmarkedLectureCard'
 import { useBookmarkedLectures } from '@/hooks/api'
-import { BookmarkIcon } from 'lucide-react'
-import { Link } from 'react-router'
-
 export default function BookmarkedLecture() {
   const { isPending, data } = useBookmarkedLectures()
 
   return (
     <div>
-      <header className="flex items-center justify-between gap-2">
+      <header className="mb-6 flex items-center justify-between gap-2">
         <div className="flex flex-col gap-2">
           <h1 className="text-heading3 text-gray-900">북마크한 강의</h1>
           <span className="font-medium text-gray-600">
@@ -26,56 +24,9 @@ export default function BookmarkedLecture() {
         {isPending ? (
           [...Array(5)].map((_, i) => <ListItemSkeleton key={i} />)
         ) : data && data.results.length > 0 ? (
-          data.results.map((lecture, i) => {
-            const {
-              thumbnail_img_url: imageUrl,
-              title,
-              instructor,
-              difficulty,
-              platform,
-              original_price: originalPrice,
-              discount_price: discountPrice,
-              url_link: urlLink,
-              duration_hhmm: durationHHMM,
-            } = lecture
-
-            return (
-              <div
-                key={i}
-                className="flex items-center justify-center gap-6 rounded-xl border border-gray-200 p-6"
-              >
-                <img src={imageUrl} className="w-full max-w-40 rounded-lg" />
-                <div className="flex flex-1 items-center justify-between">
-                  <div className="flex flex-1 flex-col">
-                    <span className="text-heading5 text-gray-900">{title}</span>
-                    <span className="text-gray-600">{instructor}</span>
-                    <div className="flex items-center gap-3">
-                      <span>{platform}</span>
-                      <span>{difficulty}</span>
-                      <span>{durationHHMM}</span>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-end justify-center gap-3">
-                    <div className="flex flex-col items-end justify-start">
-                      <span className="text-lg font-bold text-gray-900">{`₩${(originalPrice - discountPrice).toLocaleString()}`}</span>
-                      {discountPrice > 0 ? (
-                        <span className="text-sm text-gray-500 line-through">{`₩${originalPrice.toLocaleString()}`}</span>
-                      ) : null}
-                    </div>
-                    <div className="flex items-center justify-center gap-2">
-                      <BookmarkIcon
-                        className="text-primary-500 h-4"
-                        fill="#eab308"
-                      />
-                      <Link to={urlLink}>
-                        <Button className="text-sm">공고 보기</Button>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )
-          })
+          data.results.map((lecture, i) => (
+            <BookmarkedLectureCard lecture={lecture} key={i} />
+          ))
         ) : (
           <EmptyDataState />
         )}
