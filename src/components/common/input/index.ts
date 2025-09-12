494aa4a03999eb941e55df5991f8d8db32a313d8
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-12 17:38:14 +0900

    ✨ Feature: input 배럴 파일에 PasswordInput 추가 (#158)

--- src/components/common/input/index.ts
+++ src/components/common/input/index.ts
@@ -3,5 +3,13 @@ import InputIcon from '@/components/common/input/InputIcon'
 import InputDescription from '@/components/common/input/InputDescription'
 import InputLabel from '@/components/common/input/InputLabel'
 import InputErrorMessage from '@/components/common/input/InputErrorMessage'
+import PasswordInput from '@/components/common/input/PasswordInput'
 
-export { Input, InputIcon, InputDescription, InputLabel, InputErrorMessage }
+export {
+  Input,
+  InputIcon,
+  InputDescription,
+  InputLabel,
+  InputErrorMessage,
+  PasswordInput,
+}
