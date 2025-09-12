be93070e8529e42f4cdc206fc4b555bcb7851f43
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-12 17:37:22 +0900

    ✨ Feature: 비밀번호 전용 입력 필드 컴폰넌트 추가 (#158)
    비밀번호 입력 필드의 아이콘을 버튼으로 감싸야 하기 때문에 구조를 다르게 하기 위해 분리

---
+++ src/components/common/input/PasswordInput.tsx
@@ -0,0 +1,43 @@
+import type { InputProps } from '@/types'
+import { cn } from '@/utils'
+import { iconVariants, inputVariants } from './input.styles'
+import { useState } from 'react'
+import { Eye, EyeOff } from 'lucide-react'
+
+function PasswordInput({ isError = false, className, ...props }: InputProps) {
+  const [isClicked, setIsClicked] = useState(false)
+  const inputType = isClicked ? 'text' : 'password'
+
+  const handleIconClick = () => {
+    setIsClicked((prev) => !prev)
+  }
+
+  return (
+    <div className="relative w-full">
+      <input
+        type={inputType}
+        className={cn(
+          inputVariants({ isError, iconPosition: 'end' }),
+          className
+        )}
+        {...props}
+      />
+      <button
+        type="button"
+        className="cursor-pointer"
+        onClick={handleIconClick}
+      >
+        {isClicked && (
+          <EyeOff
+            className={cn(iconVariants({ iconPosition: 'end' }), 'h-5')}
+          />
+        )}
+        {!isClicked && (
+          <Eye className={cn(iconVariants({ iconPosition: 'end' }), 'h-5')} />
+        )}
+      </button>
+    </div>
+  )
+}
+
+export default PasswordInput
