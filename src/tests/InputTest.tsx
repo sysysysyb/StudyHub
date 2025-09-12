f436bc70cf0073ab0e0faa58ef9db5864f8559f7
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-12 17:39:01 +0900

    ♻️ Refactor: 수정된 입력 필드를 반영하여 테스트 페이지 수정 (#158)

--- src/tests/InputTest.tsx
+++ src/tests/InputTest.tsx
@@ -3,8 +3,8 @@ import {
   Input,
   InputDescription,
   InputErrorMessage,
-  InputIcon,
   InputLabel,
+  PasswordInput,
 } from '@/components/common/input'
 
 import { Mail } from 'lucide-react'
@@ -15,20 +15,24 @@ function InputTest() {
       <h3 className="text-center text-xl font-semibold">입력 필드 Test</h3>
       <article className="flex flex-col gap-6">
         <div>
-          <Input placeholder="아이디를 입력하세요" />
+          <Input placeholder="텍스트를 입력하세요" />
         </div>
         <div className="flex flex-col gap-2">
-          <InputLabel isRequired>이메일</InputLabel>
-          <div className="relative">
-            <InputIcon icon={Mail} className="absolute" />
-            <Input type="email" placeholder="이메일을 입력하세요" hasIcon />
-          </div>
+          <Input type="password" placeholder="텍스트를 입력하세요" isError />
+          <InputErrorMessage>잘못된 입력 형식입니다</InputErrorMessage>
+        </div>
+        <div className="flex flex-col gap-2">
+          <InputLabel>이메일</InputLabel>
+          <Input
+            type="email"
+            placeholder="이메일을 입력하세요"
+            icon={Mail}
+            iconPosition="start"
+          />
         </div>
         <div className="flex flex-col gap-2">
           <InputLabel isRequired>비밀번호</InputLabel>
-
-          <Input type="password" placeholder="비밀번호를 입력하세요" isError />
-          <InputErrorMessage>잘못된 비밀번호 형식입니다</InputErrorMessage>
+          <PasswordInput placeholder="비밀번호를 입력하세요" />
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex gap-4">
