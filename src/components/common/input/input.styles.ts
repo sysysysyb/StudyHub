d9e298bb4b20f0ae75f06471c2cfbdebc726ddb0
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-15 11:32:10 +0900

    ✨ Feature: cva를 이용한 input과 icon의 스타일 코드 분리 (#158)

--- src/components/common/input/input.styles.ts
+++ src/components/common/input/input.styles.ts
@@ -1,21 +1,28 @@
-export const InputBase =
-  'w-full focus:ring-primary-500 rounded-lg py-[13px] text-sm ring ring-gray-300 outline-none placeholder:text-gray-400 focus:border-none focus:ring-2' as const
+import {
+  IconBase,
+  IconPosition,
+  InputBase,
+  InputError,
+  InputIconPosition,
+} from '@/constants/input-variants'
+import { cva } from 'class-variance-authority'
 
-export const InputError = {
-  true: 'ring-danger-100',
-  false: 'ring-gray-300 focus:ring-primary-500 focus:border-none focus:ring-2',
-} as const
+export const inputVariants = cva(InputBase, {
+  variants: {
+    isError: InputError,
+    iconPosition: InputIconPosition,
+  },
+  defaultVariants: {
+    isError: false,
+    iconPosition: 'none',
+  },
+})
 
-export const InputIconPosition = {
-  none: 'px-4',
-  start: 'pl-10 pr-4',
-  end: 'pl-4 pr-10',
-} as const
-
-export const IconBase = 'absolute inset-y-0 my-auto h-4 text-gray-400' as const
-
-export const IconPosition = {
-  none: '',
-  start: 'left-2',
-  end: 'right-2',
-} as const
+export const iconVariants = cva(IconBase, {
+  variants: {
+    iconPosition: IconPosition,
+  },
+  defaultVariants: {
+    iconPosition: 'none',
+  },
+})
