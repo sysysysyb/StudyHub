dfc27bed4977ae661ad97ed4b5b09d747dee61a2
dirage1 <dirage4635@gmail.com>
2025-09-15 18:55:15 +0900

    ✏️ Types: MockHandler의 호출 파라미터 타입 지정 추가 (#154)

--- src/mocks/handlers/applied-recruitments-handler.ts
+++ src/mocks/handlers/applied-recruitments-handler.ts
@@ -3,7 +3,7 @@ import { http, HttpResponse } from 'msw'
 import { appliedRecruitmentsMock } from '@/mocks/data/applied-recruitments-data'
 import { applicationsMock } from '@/mocks/data/applications'
 
-const getAppliedRecruitments = http.get(
+const getAppliedRecruitments = http.get<{ application_id: string }>(
   `${API_BASE_URL}/applications/me`,
   () => {
     return HttpResponse.json(appliedRecruitmentsMock)
