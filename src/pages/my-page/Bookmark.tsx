56c658f95b8a560f15427fe6976a48c948e576ee
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-15 10:11:27 +0900

    ✨ Feature: 윈도우 사이즈에 따른 다른 컴포넌트 렌더링 (#164)

--- src/pages/my-page/Bookmark.tsx
+++ src/pages/my-page/Bookmark.tsx
@@ -3,7 +3,21 @@ import {
   BookmarkedLecture,
   BookmarkedRecruitment,
 } from '@/components/my-page/bookmarked-pages'
+import { MD_WIDTH_PIXEL } from '@/constants/break-points'
+import useWindowWidth from '@/hooks/useWindowWidth'
+import { useParams } from 'react-router'
 
 export default function Bookmark() {
-  return <div>Bookmark</div>
+  const windowWidth = useWindowWidth()
+  const { content } = useParams()
+
+  if (windowWidth < MD_WIDTH_PIXEL) {
+    return <BookmarkedContent />
+  } else {
+    if (content === 'recruitment') {
+      return <BookmarkedRecruitment />
+    } else {
+      return <BookmarkedLecture />
+    }
+  }
 }
