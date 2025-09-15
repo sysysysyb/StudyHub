649d441e93f5f1a0746d161b9ea783c5a2f98e04
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-15 11:06:42 +0900

    ♻️ Refactor: 컨벤션에 맞게 styles 파일 수정 (#158)

--- src/components/common/input/input.styles.ts
+++ src/components/common/input/input.styles.ts
@@ -1,39 +1,21 @@
-import { cva } from 'class-variance-authority'
+export const InputBase =
+  'w-full focus:ring-primary-500 rounded-lg py-[13px] text-sm ring ring-gray-300 outline-none placeholder:text-gray-400 focus:border-none focus:ring-2' as const
 
-export const inputVariants = cva(
-  'w-full focus:ring-primary-500 rounded-lg py-[13px] text-sm ring ring-gray-300 outline-none placeholder:text-gray-400 focus:border-none focus:ring-2',
-  {
-    variants: {
-      isError: {
-        true: 'ring-danger-100',
-        false:
-          'ring-gray-300 focus:ring-primary-500 focus:border-none focus:ring-2',
-      },
-      iconPosition: {
-        none: 'px-[17px]',
-        start: 'pl-10 pr-[17px]',
-        end: 'pl-[17px] pr-10',
-      },
-    },
-    defaultVariants: {
-      isError: false,
-      iconPosition: 'none',
-    },
-  }
-)
+export const InputError = {
+  true: 'ring-danger-100',
+  false: 'ring-gray-300 focus:ring-primary-500 focus:border-none focus:ring-2',
+} as const
 
-export const iconVariants = cva(
-  'absolute inset-y-0 my-auto h-4 text-gray-400',
-  {
-    variants: {
-      iconPosition: {
-        none: '',
-        start: 'left-2',
-        end: 'right-2',
-      },
-    },
-    defaultVariants: {
-      iconPosition: 'none',
-    },
-  }
-)
+export const InputIconPosition = {
+  none: 'px-4',
+  start: 'pl-10 pr-4',
+  end: 'pl-4 pr-10',
+} as const
+
+export const IconBase = 'absolute inset-y-0 my-auto h-4 text-gray-400' as const
+
+export const IconPosition = {
+  none: '',
+  start: 'left-2',
+  end: 'right-2',
+} as const
