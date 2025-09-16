2348f0803c3c04c7ae41264dadb506fe83727dcf
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-16 11:09:54 +0900

    ✨ Feature: 알림 테스트 페이지 (#133)

---
+++ src/tests/NotificationTest.tsx
@@ -0,0 +1,10 @@
+import { Notification } from '@/components'
+
+export default function NotificationTest() {
+  return (
+    <div>
+      <h1 className="text-heading2">알림 테스트</h1>
+      <Notification />
+    </div>
+  )
+}
