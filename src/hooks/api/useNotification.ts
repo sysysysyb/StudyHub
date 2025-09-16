a162267a032421720bba7143af840946a035da5e
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-16 11:12:46 +0900

    ✨ Feature: useNotification api 호출 훅 (#133)

---
+++ src/hooks/api/useNotification.ts
@@ -0,0 +1,18 @@
+import { API_BASE_URL } from '@/constants/api-constants'
+import type { NotificationList } from '@/types/api-response-types/notification-response-types'
+import api from '@/utils/axios'
+import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
+
+export default function useNotification(
+  options?: UseQueryOptions<NotificationList>
+) {
+  return useQuery<NotificationList>({
+    queryKey: ['notifications'],
+    queryFn: async () => {
+      const res = await api.get(`${API_BASE_URL}/notifications`)
+
+      return res.data
+    },
+    ...options,
+  })
+}
