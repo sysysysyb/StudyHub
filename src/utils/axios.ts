f28829eba247d8fe162e4d07eed6ae89d84b6e18
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-16 17:52:06 +0900

    ✨ Feature: withCredentials 추가 및 refresh 로직 임시 추가 (#122)

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,8 +3,25 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 })
 
-//TODO: 자동 토큰 refresh등 기능 추가
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     if (error.response?.status === 401) {
+//       try {
+//         await api.post('/users/auth/refresh')
+//         return api(error.config)
+//       } catch {
+//         window.location.href = '/auth/login'
+//       }
+//     }
+//     return Promise.reject(error)
+//   }
+// )
 
 export default api
