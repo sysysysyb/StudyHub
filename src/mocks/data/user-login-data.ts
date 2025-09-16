ce6406072b7a42b9c06a662155e8e50e89db447e
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-16 17:58:08 +0900

    ✨ Feature: 로그인 mockdata 추가 (#122)

---
+++ src/mocks/data/user-login-data.ts
@@ -0,0 +1,17 @@
+export const userLoginMock = [
+  {
+    id: 1,
+    email: 'qwerty@test.com',
+    password: 'Qwer1234!!',
+  },
+  {
+    id: 2,
+    email: 'asdfgh@test.com',
+    password: 'Asdf1234!!',
+  },
+  {
+    id: 3,
+    email: 'zxcvbn@test.com',
+    password: 'Zxcv1234!!',
+  },
+]
