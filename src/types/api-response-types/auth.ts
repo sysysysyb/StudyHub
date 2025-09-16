0e87230d945798d0d00fe048423b1b94521b67e3
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-16 17:57:18 +0900

    ✨ Types: 로그인 관련 타입 추가 (#122)

---
+++ src/types/api-response-types/auth.ts
@@ -0,0 +1,15 @@
+export interface UserInformation {
+  id: number
+  email: string
+  nickname: string
+  name: string
+  phoneNumber: string
+  birthday: string
+  gender: 'male' | 'female'
+  profileImageUrl?: string
+}
+
+export interface UserLogin {
+  email: string
+  password: string
+}
