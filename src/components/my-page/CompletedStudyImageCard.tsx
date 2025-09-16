e34e3d41eb41f0163d4437156078b7c7c1bc9b84
dirage1 <dirage4635@gmail.com>
2025-09-16 14:12:55 +0900

    ✨ Feature: 헤더 생성 및 완료된 스터디  이미지카드 컴포넌트 생성 (#176)

---
+++ src/components/my-page/CompletedStudyImageCard.tsx
@@ -0,0 +1,57 @@
+import { ImageCard, Button } from '@/components'
+import { Clock, Calendar, Star, SquarePen as EditButton } from 'lucide-react'
+
+interface CompletedStudyImageCardProps {
+  imageUrl: string
+  title: string
+  peroid: string
+  endDate: Date
+  participants: number
+  isReviewed: boolean
+  Rating: number
+  Comment: string
+}
+
+export const CompletedStudyImageCard = ({
+  imageUrl,
+  title,
+  peroid,
+  endDate,
+  participants,
+  isReviewed,
+  Rating,
+  Comment,
+}: CompletedStudyImageCardProps) => {
+  return (
+    <ImageCard imageUrl={imageUrl}>
+      <h3>{title}</h3>
+      <p>
+        <Clock /> 기간: {peroid}
+      </p>
+      <p>
+        <Calendar /> 종료: {endDate}
+      </p>
+      <p>{participants}</p>
+
+      {isReviewed ? (
+        <>
+          <div className="flex justify-between">
+            <div className="flex">
+              <Star />
+              {Rating}
+            </div>
+            <EditButton />
+          </div>
+          <p>{Comment}</p>
+        </>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <div className="bg-primary-50 text-primary-800 rounded-md p-4">
+            아직 리뷰를 작성하지 않았습니다.
+          </div>
+          <Button className="w-full">리뷰 작성</Button>
+        </div>
+      )}
+    </ImageCard>
+  )
+}
--- src/pages/my-page/CompletedStudy.tsx
+++ src/pages/my-page/CompletedStudy.tsx
@@ -1,3 +1,13 @@
 export const CompletedStudy = () => {
-  return <></>
+  return (
+    <main>
+      <header className="gap-2 pb-6">
+        <h3 className="text-heading3">지원 내역</h3>
+        <span className="text-secondary">
+          내가 지원한 스터디 구인 공고들을 확인하세요
+        </span>
+      </header>
+      <section className="grid-cols-1 sm:grid-cols-2"></section>
+    </main>
+  )
 }
