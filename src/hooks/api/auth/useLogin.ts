662d7871cd93ee99fe099333335df2ce953a2719
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-18 13:58:27 +0900

    ♻️ Refactor: useLogin 훅 수정 (#122)
    api 명세서에 맞게 쿼리키 수정 / 로그인 성공 시 세션 스토리지에 로그인 성공 상태 저장하는 함수 적용 / 에러 발생 시 토스트 메시지 적용

--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,5 +1,6 @@
 import useToast from '@/hooks/useToast'
 import type { UserLogin } from '@/types/api-response-types/auth'
+import { setIsLoggedIn } from '@/utils'
 import api from '@/utils/axios'
 import {
   useMutation,
@@ -14,13 +15,17 @@ export default function useLogin(
   const { triggerToast } = useToast()
 
   return useMutation<void, Error, UserLogin>({
-    mutationKey: ['users', 'auth', 'login'],
+    mutationKey: ['auth', 'email', 'login'],
     mutationFn: async (payload) => {
-      await api.post(`/users/auth/login`, payload)
+      await api.post(`/auth/email/login`, payload)
     },
     onSuccess: async () => {
+      setIsLoggedIn()
       await qc.invalidateQueries({ queryKey: ['users', 'me'] })
       triggerToast('success', '로그인 성공')
+    },
+    onError: () => {
+      triggerToast('error', '잘못된 이메일 또는 비밀번호 입니다.')
     },
     ...options,
   })
