470cc57fbd45c2f475bdb8f552172bbbc917e1c1
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-18 14:19:27 +0900

    ✨ Feature: useInfiniteBookmarkedRecruitment api 호출 훅 (#170)

--- src/tests/BookmarkedRecruitmentInfitinyScrollTest.tsx
+++ src/tests/BookmarkedRecruitmentInfitinyScrollTest.tsx
@@ -1,3 +1,31 @@
+import { BookmarkedRecruitmentCard } from '@/components/my-page'
+import useInfiniteBookmarkedRecruitment from '@/hooks/api/bookmarked/useInfiniteBookmarkedRecruitment'
+import React from 'react'
+
 export default function BookmarkedRecruitmentInfitinyScrollTest() {
-  return <div>BookmarkedRecruitmentTest</div>
+  const { data, fetchNextPage } = useInfiniteBookmarkedRecruitment()
+
+  return (
+    <div className="flex flex-col gap-2">
+      {data
+        ? data.pages.map((group, i) => (
+            <React.Fragment key={i}>
+              {group.results.map((recruitment) => (
+                <BookmarkedRecruitmentCard
+                  key={recruitment.uuid}
+                  recruitment={recruitment}
+                />
+              ))}
+            </React.Fragment>
+          ))
+        : '로딩중'}
+      <button
+        onClick={() => {
+          fetchNextPage()
+        }}
+      >
+        새 페이지
+      </button>
+    </div>
+  )
 }
