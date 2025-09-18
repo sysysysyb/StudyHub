dee2d396822bc57ca18e17ee8f7352ff303f220a
dirage1 <dirage4635@gmail.com>
2025-09-18 14:37:37 +0900

    🩹 Fix: 절대경로 추가 (#181)

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -4,7 +4,7 @@ import useChatMessages from '@/hooks/api/chat/useChatMessages'
 import useAppliedRecruitment from '@/hooks/api/useAppliedRecruitment'
 import useBookmarkedLectures from '@/hooks/api/useBookmarkedLecture'
 import useCompletedStudy from '@/hooks/api/useCompletedStudy'
-import useApplicationDetail from './useApplicationDetail'
+import useApplicationDetail from '@/hooks/api/useApplicationDetail'
 
 export {
   useBookmarkedRecruitment,
