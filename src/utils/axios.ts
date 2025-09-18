b88e7fc73ec40605aade5c17a072c6a4a925230e
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-18 14:49:51 +0900

    💡 Comment: api 토큰 리프레시 interceptor 로직 주석 삭제 (#122)
    주석으로 임시 추가 후 서버 연결 시 약간의 수정을 거쳐 사용하려 했는데 api 명세서를 보니 엔드포인트 등 전체적으로 수정 중인 것 같아 삭제하고 이후 다시 추가 예정

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -9,19 +9,4 @@ const api = axios.create({
   withCredentials: true,
 })
 
-// api.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     if (error.response?.status === 401) {
-//       try {
-//         await api.post('/users/auth/refresh')
-//         return api(error.config)
-//       } catch {
-//         window.location.href = '/auth/login'
-//       }
-//     }
-//     return Promise.reject(error)
-//   }
-// )
-
 export default api
