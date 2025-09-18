e26320388fe016dd37e27cdc27a3fe2695412b51
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-18 18:45:04 +0900

    ♻️ Refactor: auth 관련 msw 핸들러 수정 (#122)
    msw 적용을 위해 쿠키에 임시로 담을 access-token과 refresh-token 추가 / 로그인 시 쿠키에 두 토큰이 담기도록 수정 / 로그아웃 시 두 토큰이 삭제되도록 수정 / 사용자 정보를 가져올 때 액세스 토큰이 존재하는 지 체크 / 액세스 토큰 재발급 msw 핸들러 추가

--- src/mocks/handlers/auth-handler.ts
+++ src/mocks/handlers/auth-handler.ts
@@ -1,8 +1,11 @@
 import { API_BASE_URL } from '@/constants/api-constants'
 import { http, HttpResponse, passthrough } from 'msw'
 import { userInformationMock } from '@/mocks/data/user-information-data'
-import { getIsLoggedIn } from '@/utils'
 import { loginSchema } from '@/schemas/form-schema/auth-schema'
+
+const ACCESS_TOKEN = `msw-access-token=access-token-test; Path=/; SameSite=Strict;`
+const REFRESH_TOKEN =
+  'msw-refresh-token=refresh-token-test; Path=/; SameSite=Strict;'
 
 const login = http.post(
   `${API_BASE_URL}/auth/email/login`,
@@ -22,7 +25,16 @@ const login = http.post(
     const data = parsedBody.data
 
     if (data.email === 'qwerty@test.com' && data.password === 'Qwer1234!!') {
-      return HttpResponse.json({ message: 'Login successful' }, { status: 200 })
+      return HttpResponse.json(
+        { message: 'Login successful' },
+        {
+          status: 200,
+          headers: [
+            ['Set-Cookie', `${ACCESS_TOKEN} Max-Age=10`],
+            ['Set-Cookie', `${REFRESH_TOKEN} Max-Age=360000`],
+          ],
+        }
+      )
     } else {
       return HttpResponse.json(
         {
@@ -35,17 +47,49 @@ const login = http.post(
 )
 
 const logout = http.post(`${API_BASE_URL}/users/logout`, () => {
-  return HttpResponse.json({ message: 'Logout successful' }, { status: 200 })
+  return HttpResponse.json(
+    { message: 'Logout successful' },
+    {
+      status: 200,
+      headers: [
+        ['Set-Cookie', `${ACCESS_TOKEN} MAX-Age=0`],
+        ['Set-Cookie', `${REFRESH_TOKEN} MAX-Age=0`],
+      ],
+    }
+  )
 })
 
 const getUserInformation = http.get(`${API_BASE_URL}/users/me`, () => {
-  if (!getIsLoggedIn()) {
+  const currentCookie = document.cookie
+  const isAccessTokenRemain = currentCookie.includes('access-token')
+
+  if (!isAccessTokenRemain) {
     return HttpResponse.json(
       { message: 'Authentication required.' },
       { status: 401 }
     )
   }
   return HttpResponse.json(userInformationMock[0])
+})
+
+const getRefreshToken = http.post(`${API_BASE_URL}/token/refresh`, () => {
+  const currentCookie = document.cookie
+  const isRefreshTokenRemain = currentCookie.includes('refresh-token')
+
+  if (!isRefreshTokenRemain) {
+    return HttpResponse.json(
+      { message: '리프레시 토큰이 만료됐습니다' },
+      { status: 401 }
+    )
+  }
+
+  return HttpResponse.json(
+    { message: '새로운 액세스 토큰을 발급했습니다' },
+    {
+      status: 200,
+      headers: [['Set-Cookie', ACCESS_TOKEN]],
+    }
+  )
 })
 
 const passthroughPiscumPhotos = http.get(
@@ -59,5 +103,6 @@ export const authHandlers = [
   login,
   logout,
   getUserInformation,
+  getRefreshToken,
   passthroughPiscumPhotos,
 ]
