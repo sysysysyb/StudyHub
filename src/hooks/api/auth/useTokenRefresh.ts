a58946e2385df73e6cdf6c814c42a8f536e12004
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-20 04:56:23 +0900

    ♻️ Refactor: useTokenRefresh 수정 (#199)
    토큰 재발급 실패 시 로그인 여부 전역 상태 변경 / 토스트 메시지와 navigate 관련 로직 삭제

--- src/hooks/api/auth/useTokenRefresh.ts
+++ src/hooks/api/auth/useTokenRefresh.ts
@@ -1,16 +1,14 @@
-import useToast from '@/hooks/useToast'
+import { useLoginStore } from '@/store/useLoginStore'
 import api from '@/utils/axios'
 import {
   useMutation,
   useQueryClient,
   type UseMutationOptions,
 } from '@tanstack/react-query'
-import { useNavigate } from 'react-router'
 
 export default function useTokenRefresh(options?: UseMutationOptions) {
   const qc = useQueryClient()
-  const navigate = useNavigate()
-  const { triggerToast } = useToast()
+  const { setLoggedOut } = useLoginStore()
 
   return useMutation({
     ...options,
@@ -21,10 +19,8 @@ export default function useTokenRefresh(options?: UseMutationOptions) {
       return newAccessToken
     },
     onError: () => {
-      // TODO: 로그인 여부 전역 상태 false로 설정
+      setLoggedOut()
       qc.removeQueries({ queryKey: ['users', 'me'] })
-      triggerToast('error', '로그인이 만료되었습니다. 다시 로그인 해주세요.')
-      navigate('/auth/login')
     },
   })
 }
