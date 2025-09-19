05c208c58563c805497bbca2cbac9cf075a55fa0
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-20 00:18:51 +0900

    ✨ Feature: 퍼블릭 엔드포인트 상수 추가 (#199)
    액세스 토큰을 보낼 필요 없는 api 요청의 엔드포인트 정리

---
+++ src/constants/api-endpoints.ts
@@ -0,0 +1,12 @@
+export const PUBLIC_ENDPOINTS = [
+  'auth/email/login',
+  'users/signup',
+  'auth/kakao/callback',
+  'auth/naver/callback',
+  'email/send-code',
+  'phone/send-code',
+  'auth/email/verify',
+  'auth/verify-phone',
+  'token/refresh',
+  'users/recover-account',
+] as const
