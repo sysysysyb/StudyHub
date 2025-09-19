d6f521f58a2875955e75d8f194d41ac255947697
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-19 13:59:19 +0900

    ✨ Feature: types 배럴 파일에 UserInformation 추가 (#195)

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,5 +5,12 @@ import type {
   recruitmentSearchParams,
   lectureSearchParams,
 } from '@/types/api-request-types/bookmarked-request-types'
+import type { UserInformation } from '@/types/api-response-types/auth-response-types'
 
-export type { Toast, InputProps, recruitmentSearchParams, lectureSearchParams }
+export type {
+  Toast,
+  InputProps,
+  recruitmentSearchParams,
+  lectureSearchParams,
+  UserInformation,
+}
