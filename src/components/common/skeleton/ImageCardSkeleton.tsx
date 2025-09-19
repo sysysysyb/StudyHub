6d76f3f82e423a97ba5ba7145c3f25e6cf45720a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-19 14:01:32 +0900

    ♻️ Refactor: 이름 변경된 스켈레톤 item 요소 적용 (#195)

--- src/components/common/skeleton/ImageCardSkeleton.tsx
+++ src/components/common/skeleton/ImageCardSkeleton.tsx
@@ -1,14 +1,14 @@
-import { Rectangle, Sentence } from './SkeletonItem'
+import { SkeletonLine, SkeletonRectangle } from './SkeletonItem'
 
 function ImageCardSkeleton() {
   return (
     <div className="flex w-full flex-col items-center gap-2 overflow-hidden rounded-[12px] border border-solid border-gray-300">
-      <Rectangle width={100} height={60} isRounded={false} />
+      <SkeletonRectangle width={100} height={60} isRounded={false} />
       <div className="w-full flex-3 space-y-2 p-5 sm:space-y-6">
-        <Sentence />
-        <Sentence />
-        <Sentence />
-        <Rectangle width={100} height={25} />
+        <SkeletonLine />
+        <SkeletonLine />
+        <SkeletonLine />
+        <SkeletonRectangle width={100} height={25} />
       </div>
     </div>
   )
--- src/components/common/skeleton/ListItemSkeleton.tsx
+++ src/components/common/skeleton/ListItemSkeleton.tsx
@@ -1,16 +1,16 @@
-import { Rectangle, Sentence } from './SkeletonItem'
+import { SkeletonLine, SkeletonRectangle } from './SkeletonItem'
 
 function ListItemSkeleton() {
   return (
     <div className="flex w-full items-center gap-2 border border-solid border-gray-300 p-[25px]">
       <div className="flex-1">
-        <Rectangle width={100} height={50} />
+        <SkeletonRectangle width={100} height={50} />
       </div>
       <div className="w-full flex-3 space-y-2">
-        <Sentence />
-        <Sentence width={50} />
-        <Sentence width={50} />
-        <Sentence width={75} />
+        <SkeletonLine />
+        <SkeletonLine width={50} />
+        <SkeletonLine width={50} />
+        <SkeletonLine width={75} />
       </div>
     </div>
   )
