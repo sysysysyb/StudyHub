dc549033f280a6dd63b11fcc0e19e820f8ede52d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-21 03:26:34 +0900

    ♻️ Refactor: useLogin 수정 (#199)
    요청 후 응답이 돌아오면 액세스 토큰 반환 / 성공 토스트 메시지 문구 수정 / 요청 성공 시 로그인 여부 전역 상태 변경, 로컬 스토리지에 액세스 토큰 저장

--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,6 +1,7 @@
 import { useToast } from '@/hooks'
 import { useLoginStore } from '@/store/useLoginStore'
 import type { UserLogin } from '@/types/api-request-types/auth-request-types'
+import { setAccessToken } from '@/utils'
 import api from '@/utils/axios'
 import {
   useMutation,
@@ -9,25 +10,28 @@ import {
 } from '@tanstack/react-query'
 
 export default function useLogin(
-  options?: UseMutationOptions<void, Error, UserLogin>
+  options?: UseMutationOptions<string, Error, UserLogin>
 ) {
   const qc = useQueryClient()
   const { triggerToast } = useToast()
-  const { setLoggedIn } = useLoginStore()
+  const { setIsLoggedIn } = useLoginStore()
 
-  return useMutation<void, Error, UserLogin>({
+  return useMutation<string, Error, UserLogin>({
+    ...options,
     mutationKey: ['auth', 'email', 'login'],
     mutationFn: async (payload) => {
-      await api.post(`/auth/email/login`, payload)
+      const response = await api.post(`/auth/email/login`, payload)
+      const newAccessToken = response.data.access_token
+      return newAccessToken
     },
-    onSuccess: async () => {
+    onSuccess: async (newAccessToken: string) => {
+      setAccessToken(newAccessToken)
+      setIsLoggedIn(true)
       await qc.invalidateQueries({ queryKey: ['users', 'me'] })
-      setLoggedIn()
-      triggerToast('success', '로그인에 성공했습니다.')
+      triggerToast('success', '로그인이 완료되었습니다.')
     },
     onError: () => {
       triggerToast('error', '잘못된 이메일 또는 비밀번호 입니다.')
     },
-    ...options,
   })
 }
