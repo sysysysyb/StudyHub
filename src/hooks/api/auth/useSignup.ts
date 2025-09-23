bc4c5a7ad42999aee0eec9e7bfd4d1d344ca7003
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-23 21:38:38 +0900

    ✨ Feature: useSignup 훅 추가 (#190)

---
+++ src/hooks/api/auth/useSignup.ts
@@ -0,0 +1,31 @@
+import useToast from '@/hooks/useToast'
+import type { UserSignup } from '@/types/api-request-types/auth-request-types'
+import api from '@/utils/axios'
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
+
+export default function useSignup(
+  options?: UseMutationOptions<unknown, AxiosError, UserSignup>
+) {
+  const { triggerToast } = useToast()
+
+  return useMutation<unknown, AxiosError, UserSignup>({
+    ...options,
+    mutationKey: ['auth', 'email', 'signup'],
+    mutationFn: async (payload) => {
+      await api.post(`auth/email/signup`, payload)
+    },
+    onSuccess: () => {
+      triggerToast('success', 'Signup ✨', '회원가입을 완료했습니다')
+    },
+    onError: (error) => {
+      const status = error.status
+
+      if (status === 400) {
+        triggerToast('error', '잘못 입력된 항목이 있습니다')
+      } else {
+        triggerToast('error', '잠시 후 다시 시도해주세요')
+      }
+    },
+  })
+}
