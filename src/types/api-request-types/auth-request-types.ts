d5921ce57356eb985188a97d97919035a045191f
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-23 21:36:18 +0900

    ✨ Types: auth 관련 request 타입 추가 (#190)

--- src/types/api-request-types/auth-request-types.ts
+++ src/types/api-request-types/auth-request-types.ts
@@ -2,3 +2,33 @@ export interface UserLogin {
   email: string
   password: string
 }
+
+export interface UserSignup {
+  email: string
+  password: string
+  nickname: string
+  name: string
+  phone_number: string
+  birthday: string
+  gender: string
+  email_verification_code: string
+  phone_verification_code: string
+}
+
+export interface UserEmailSendCode {
+  email: string
+}
+
+export interface UserEmailVerify {
+  email: string
+  verificationCode: string
+}
+
+export interface UserPhoneSendCode {
+  phoneNumber: string
+}
+
+export interface UserPhoneVerify {
+  phoneNumber: string
+  verificationCode: string
+}
