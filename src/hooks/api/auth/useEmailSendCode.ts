54ca584eeed903897b17fc8a7de984be4294e684
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-23 21:36:48 +0900

    ✨ Feature: useEmailSendCode 훅 추가 (#190)

---
+++ src/hooks/api/auth/useEmailSendCode.ts
@@ -0,0 +1,35 @@
+import useToast from '@/hooks/useToast'
+import type { UserEmailSendCode } from '@/types/api-request-types/auth-request-types'
+import api from '@/utils/axios'
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
+
+export default function useEmailSendCode(
+  options?: UseMutationOptions<unknown, AxiosError, UserEmailSendCode>
+) {
+  const { triggerToast } = useToast()
+
+  return useMutation<unknown, AxiosError, UserEmailSendCode>({
+    ...options,
+    mutationKey: ['auth', 'email', 'send-code'],
+    mutationFn: async (payload) => {
+      await api.post(`auth/email/send-code`, payload)
+    },
+    onSuccess: () => {
+      triggerToast(
+        'success',
+        '이메일 인증 코드를 전송했습니다 ✉️',
+        '확인 후 입력해주세요'
+      )
+    },
+    onError: (error) => {
+      const status = error.status
+
+      if (status === 400) {
+        triggerToast('error', '잘못된 이메일 형식입니다')
+      } else {
+        triggerToast('error', '잠시 후 다시 시도해주세요')
+      }
+    },
+  })
+}
