ceb2b4df587f3eebf66d3366e8244e9ad63c7967
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-23 21:37:33 +0900

    ✨ Feature: usePhoneSendCode 훅 추가 (#190)

---
+++ src/hooks/api/auth/usePhoneSendCode.ts
@@ -0,0 +1,35 @@
+import useToast from '@/hooks/useToast'
+import type { UserPhoneSendCode } from '@/types/api-request-types/auth-request-types'
+import api from '@/utils/axios'
+import { useMutation, type UseMutationOptions } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
+
+export default function usePhoneSendCode(
+  options?: UseMutationOptions<unknown, AxiosError, UserPhoneSendCode>
+) {
+  const { triggerToast } = useToast()
+
+  return useMutation<unknown, AxiosError, UserPhoneSendCode>({
+    ...options,
+    mutationKey: ['auth', 'phone', 'send-code'],
+    mutationFn: async (payload) => {
+      await api.post(`auth/phone/send-code`, payload)
+    },
+    onSuccess: () => {
+      triggerToast(
+        'success',
+        '휴대전화 번호 인증 코드를 전송했습니다 ✉️',
+        '확인 후 입력해주세요'
+      )
+    },
+    onError: (error) => {
+      const status = error.status
+
+      if (status === 400) {
+        triggerToast('error', '잘못된 휴대전화 번호 형식입니다')
+      } else {
+        triggerToast('error', '잠시 후 다시 시도해주세요')
+      }
+    },
+  })
+}
