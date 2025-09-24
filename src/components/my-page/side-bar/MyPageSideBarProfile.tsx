318e6db5a9183acfe4abead83dc3e72c5aeab6cf
dirage1 <dirage4635@gmail.com>
2025-09-24 11:26:01 +0900

    ✨ Feature: 마이페이지 사이드바 프로필도 api 로직 적용 (#215)

--- src/components/my-page/side-bar/MyPageSideBarProfile.tsx
+++ src/components/my-page/side-bar/MyPageSideBarProfile.tsx
@@ -1,34 +1,23 @@
 import { Avatar } from '@/components'
-
-//임시 유저 정보 인터페이스 (추후 삭제)
-interface UserInformation {
-  username: string
-  email: string
-  signUpDay: Date
-  profileImageUrl?: string
-}
-
-//임시 유저 정보 (추후 삭제)
-const dummyUserInfo: UserInformation = {
-  username: 'test1234',
-  email: 'test@test.com',
-  signUpDay: new Date(),
-  profileImageUrl: 'https://dummyimage.com/600x400/000/fff',
-}
+import { useUserInformation } from '@/hooks/api'
 
 export default function MyPageSideBarProfile() {
-  //TODO: 나중에 전역상태에서 실제 유저 데이터 불러오기
-  const { username, email, signUpDay, profileImageUrl } = dummyUserInfo
+  const { data: userInfo } = useUserInformation()
+  if (!userInfo) return '로그인이 필요합니다.'
+  const signUpDate = new Date(userInfo.birthday)
+  const signUpYear = signUpDate.getFullYear()
+  const signUpMonth = signUpDate.getMonth()
 
-  const signUpYear = signUpDay.getFullYear()
-  const signUpMonth = signUpDay.getMonth()
-
-  //16 4 8
   return (
     <div className="flex flex-col items-center justify-start">
-      <Avatar src={profileImageUrl} size="2xl" state="none" className="mb-4" />
-      <span className="text-heading5 mb-1 text-gray-900">{username}</span>
-      <span className="text-secondary mb-2">{email}</span>
+      <Avatar
+        src={userInfo.profileImageUrl}
+        size="2xl"
+        state="none"
+        className="mb-4"
+      />
+      <span className="text-heading5 mb-1 text-gray-900">{userInfo.name}</span>
+      <span className="text-secondary mb-2">{userInfo.email}</span>
       <span className="text-xs text-gray-500">{`가입일: ${signUpYear}년 ${signUpMonth}월`}</span>
     </div>
   )
