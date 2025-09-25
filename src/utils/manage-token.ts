d07af25f56be663fe07d3d80537a6a11954e191d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-25 14:22:29 +0900

    ♻️ Refactor: Refactor: 토큰 관련 유틸 수정 (#241)
    api 명세서에 따라 access_token을 access로 수정

--- src/utils/manage-token.ts
+++ src/utils/manage-token.ts
@@ -1,11 +1,11 @@
 export function getAccessToken() {
-  return localStorage.getItem('access-token')
+  return localStorage.getItem('access')
 }
 
 export function setAccessToken(token: string) {
-  localStorage.setItem('access-token', token)
+  localStorage.setItem('access', token)
 }
 
 export function clearAccessToken() {
-  localStorage.removeItem('access-token')
+  localStorage.removeItem('access')
 }
