b60359ac3506e1c31a2d3a50e4604cf03531172c
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-25 14:21:08 +0900

    ♻️ Refactor: 새로 추가한 전역 상태 배럴 파일에 추가 (#241)

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,5 +2,11 @@
 import { useToastStore } from '@/store/useToastStore'
 import { useNotificationNavigationItemStore } from '@/store/useNotificationNavigationItemStore'
 import { useChatRoomStore } from '@/store/useChatRoomStore'
+import { useWithdrawalDateStore } from './useWithdrawalDateStore'
 
-export { useToastStore, useNotificationNavigationItemStore, useChatRoomStore }
+export {
+  useToastStore,
+  useNotificationNavigationItemStore,
+  useChatRoomStore,
+  useWithdrawalDateStore,
+}
