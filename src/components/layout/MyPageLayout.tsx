7b47d1855d4284b58fed50018ee81aafa59e4450
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-25 14:52:47 +0900

    ✏️ Fix: 하위 레이아웃의 배경색 통일 (#246)

--- src/components/layout/MyPageLayout.tsx
+++ src/components/layout/MyPageLayout.tsx
@@ -8,7 +8,7 @@ export default function MyPageLayout() {
   const width = useWindowWidth()
 
   return (
-    <div className="flex flex-col items-center justify-start gap-8 bg-gray-100 p-8 md:flex-row md:items-start md:justify-center">
+    <div className="flex flex-col items-center justify-start gap-8 p-8 md:flex-row md:items-start md:justify-center">
       {width < MD_WIDTH_PIXEL ? <MobileMyPageNavBar /> : <MyPageSideBar />}
 
       <div className="min-h-[700px] max-w-4xl flex-1 rounded-lg border border-gray-200 bg-white p-8">
