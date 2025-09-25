61af44fa80ca43fe306e074e6b4235d1c99c065f
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-25 13:35:23 +0900

    💄 Design: 404 페이지 기본 디자인 (#240)

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,20 @@
 // pages/NotFound.tsx
+import { Button } from '@/components'
 import { Link } from 'react-router'
 
 export default function NotFound() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>404</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
-      <Link to="/">홈으로 돌아가기</Link>
+    <div className="flex flex-col items-start gap-8 p-10">
+      <h1 className="text-heading1">404 ERROR</h1>
+      <div className="flex flex-col gap-1">
+        <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
+        <p>존재하지 않는 주소를 입력하셨거나</p>
+        <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
+      </div>
+
+      <Button size="lg">
+        <Link to="/">홈으로 돌아가기</Link>
+      </Button>
     </div>
   )
 }
