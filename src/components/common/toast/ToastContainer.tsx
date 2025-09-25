f08263a4079a3834a84e5cba7e1d58d36679482a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-25 17:35:13 +0900

    💄 Design: ToastContainer width 스타일 수정 (#259)
    useWindowWidth를 이용해서 window width를 계산하고, 450px 이하일 경우 토스트 메시지 사이즈가 화면 전체 width - 8만큼의 사이즈를 갖도록 수정

--- src/components/common/toast/ToastContainer.tsx
+++ src/components/common/toast/ToastContainer.tsx
@@ -3,18 +3,24 @@ import ToastItem from './ToastItem'
 import { useEffect, useState } from 'react'
 import { useDebounce } from '@/hooks/useDebounce'
 import type { Toast } from '@/types'
+import { useWindowWidth } from '@/hooks'
 
 function ToastContainer() {
   const [renderedToasts, setRenderedToasts] = useState<Toast[]>([])
   const { toasts } = useToastStore()
   const debouncedToasts = useDebounce(toasts, 200)
+  const windowWidth = useWindowWidth()
+  const mobileToastContainerWidth = windowWidth - 8
 
   useEffect(() => {
     setRenderedToasts(debouncedToasts)
   }, [debouncedToasts])
 
   return (
-    <div className="fixed top-18 right-1 z-100 w-112 space-y-1">
+    <div
+      className="fixed top-18 right-1 z-100 space-y-1"
+      style={{ width: windowWidth < 450 ? mobileToastContainerWidth : '448px' }}
+    >
       {renderedToasts.map(({ id, type, title, content }) => (
         <ToastItem
           key={id}
