0d8b083c9c92ffb521a7fe220d9e89a8c2d419da
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-28 22:19:33 +0900

    ✨ Feature: 이메일 & 비밀번호 찾기 msw 핸들러 추가 (#280)
    이메일 찾기 & 비밀번호 재설정 관련 msw 핸들러 5가지 추가

--- src/mocks/handlers/auth-handler.ts
+++ src/mocks/handlers/auth-handler.ts
@@ -1,5 +1,6 @@
 import { MSW_BASE_URL } from '@/constants/url-constants'
 import { http, HttpResponse, passthrough } from 'msw'
+import { userInformationMock } from '../data/user-information-data'
 const ACCESS_TOKEN = `msw-access-token=access-token-test; Path=/; SameSite=Strict;`
 const REFRESH_TOKEN =
   'msw-refresh-token=refresh-token-test; Path=/; SameSite=Strict;'
@@ -206,6 +207,138 @@ const signup = http.post(
   }
 )
 
+// 이메일 찾기 - 휴대전화 인증코드 전송
+const findEmailSendCode = http.post(
+  `${MSW_BASE_URL}/auth/find-email/send`,
+  async ({ request }) => {
+    const body = await request.clone().json()
+
+    if (!body.phone_number) {
+      return HttpResponse.json(
+        {
+          error: '잘못된 입력 형식입니다',
+        },
+        { status: 400 }
+      )
+    }
+
+    return HttpResponse.json(
+      {
+        detail: '휴대전화 인증코드를 전송했습니다',
+      },
+      {
+        status: 200,
+      }
+    )
+  }
+)
+
+// 이메일 찾기 - 휴대전화 인증코드 검증
+const findEmailVerify = http.post(
+  `${MSW_BASE_URL}/auth/find-email/verify`,
+  async ({ request }) => {
+    const body = await request.clone().json()
+
+    if (!body.name || !body.phone_number || !body.verification_code) {
+      return HttpResponse.json(
+        {
+          error: '잘못된 입력 형식입니다',
+        },
+        { status: 400 }
+      )
+    }
+
+    return HttpResponse.json(
+      {
+        detail: '휴대전화 인증코드가 확인되었습니다',
+        email: userInformationMock[0].email,
+      },
+      {
+        status: 200,
+      }
+    )
+  }
+)
+
+// 비밀번호 찾기 - 이메일 인증코드 전송
+const resetPasswordSendCode = http.post(
+  `${MSW_BASE_URL}/auth/reset-password/send`,
+  async ({ request }) => {
+    const body = await request.clone().json()
+
+    if (!body.email) {
+      return HttpResponse.json(
+        {
+          error: '잘못된 입력 형식입니다',
+        },
+        { status: 400 }
+      )
+    }
+
+    return HttpResponse.json(
+      {
+        detail: '이메일 인증코드를 전송했습니다',
+      },
+      {
+        status: 200,
+      }
+    )
+  }
+)
+
+// 비밀번호 찾기 - 이메일 인증코드 검증
+const resetPasswordVerify = http.post(
+  `${MSW_BASE_URL}/auth/reset-password/verify`,
+  async ({ request }) => {
+    const body = await request.clone().json()
+
+    if (!body.email || !body.verification_code) {
+      return HttpResponse.json(
+        {
+          error: '잘못된 입력 형식입니다',
+        },
+        { status: 400 }
+      )
+    }
+
+    return HttpResponse.json(
+      {
+        detail: '이메일 인증코드가 확인되었습니다',
+        email: userInformationMock[0].email,
+      },
+      {
+        status: 200,
+      }
+    )
+  }
+)
+
+// 새 비밀번호 설정
+const resetPassword = http.patch(
+  `${MSW_BASE_URL}/auth/reset-password`,
+  async ({ request }) => {
+    const body = await request.clone().json()
+
+    if (!body.password) {
+      return HttpResponse.json(
+        {
+          error: '잘못된 입력 형식입니다',
+        },
+        { status: 400 }
+      )
+    }
+
+    return HttpResponse.json(
+      {
+        detail: '새 비밀번호 설정이 완료되었습니다',
+      },
+      {
+        status: 200,
+      }
+    )
+  }
+)
+
 // 외부 이미지 api
 const passthroughPiscumPhotos = http.get(
   `https://picsum.photos/id/:rest*`,
@@ -223,5 +356,10 @@ export const authHandlers = [
   phoneSendCode,
   phoneVerify,
   signup,
+  findEmailSendCode,
+  findEmailVerify,
+  resetPasswordSendCode,
+  resetPasswordVerify,
+  resetPassword,
   passthroughPiscumPhotos,
 ]
