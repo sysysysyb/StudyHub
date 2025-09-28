e51d16fcb2bdfabd6b7d580888b71eac73fdff6d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-28 19:47:00 +0900

    🚚 Rename: import 절대 경로로 수정 (#277)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import useAxiosInterceptor from '@/hooks/api/useAxiosInterceptor'
 import { useEffect } from 'react'
-import { getAccessToken } from './utils'
-import useTokenRefresh from './hooks/api/auth/useTokenRefresh'
-import MainRoutes from './routes/MainRoutes'
+import { getAccessToken } from '@/utils'
+import useTokenRefresh from '@/hooks/api/auth/useTokenRefresh'
+import MainRoutes from '@/routes/MainRoutes'
 
 function App() {
   const refreshToken = useTokenRefresh()
