18ce2fe0300c2c58e6e70ba96916b0f82921535f
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-29 22:39:22 +0900

    ♻️ Refactor: MainRoutes에 lazy loading 적용 (#269)
    RootLayout과 랜딩 페이지를 제외한 나머지 페이지 모두 lazy 적용

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,53 +1,64 @@
-import { AuthLayout, MyPageLayout, RootLayout } from '@/components'
-import {
-  AppliedRecruitment,
-  LandingPage,
-  Login,
-  MyInfo,
-  Signup,
-  CompletedStudy,
-  Bookmark,
-  FindEmail,
-  FindPassword,
-  NotFound,
-} from '@/pages'
-import KakaoAuth from '@/pages/auth/KakaoAuth'
+import RootLayout from '@/components/layout/RootLayout'
 import { Route, Routes } from 'react-router'
-import TestRoutes from './TestRoutes'
+import { lazy, Suspense } from 'react'
+import LandingPage from '@/pages/LandingPage'
+
+// layouts
+const AuthLayout = lazy(() => import('@/components/layout/AuthLayout'))
+const MyPageLayout = lazy(() => import('@/components/layout/MyPageLayout'))
+
+// routes
+const TestRoutes = lazy(() => import('@/routes/TestRoutes'))
+
+// pages
+const AppliedRecruitment = lazy(
+  () => import('@/pages/my-page/AppliedRecruitment')
+)
+const Login = lazy(() => import('@/pages/auth/Login'))
+const MyInfo = lazy(() => import('@/pages/my-page/MyInfo'))
+const Signup = lazy(() => import('@/pages/auth/SignUp'))
+const CompletedStudy = lazy(() => import('@/pages/my-page/CompletedStudy'))
+const Bookmark = lazy(() => import('@/pages/my-page/Bookmark'))
+const FindEmail = lazy(() => import('@/pages/auth/FindEmail'))
+const FindPassword = lazy(() => import('@/pages/auth/FindPassword'))
+const NotFound = lazy(() => import('@/pages/NotFound'))
+const KakaoAuth = lazy(() => import('@/pages/auth/KakaoAuth'))
 
 function MainRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<LandingPage />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<LandingPage />} />
 
-        <Route path="auth" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="find-email" element={<FindEmail />} />
-          <Route path="find-password" element={<FindPassword />} />
-          <Route path="restore-user" element={<div>restore-user</div>} />
+          <Route path="auth" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="find-email" element={<FindEmail />} />
+            <Route path="find-password" element={<FindPassword />} />
+            <Route path="restore-user" element={<div>restore-user</div>} />
+          </Route>
+
+          <Route path="my-page" element={<MyPageLayout />}>
+            <Route index element={<MyInfo />} />
+            <Route path="applications" element={<AppliedRecruitment />} />
+            <Route path="completed-study" element={<CompletedStudy />} />
+            <Route path="bookmarked/" element={<Bookmark />} />
+            <Route path="bookmarked/:content" element={<Bookmark />} />
+          </Route>
+
+          {/* 404 페이지 */}
+          <Route path="*" element={<NotFound />} />
+
+          {/* 소셜 로그인 */}
+          <Route path="/oauth/kakao" element={<KakaoAuth />} />
+          {/* <Route path="/oauth/naver" element={<NaverAuth />} /> */}
         </Route>
 
-        <Route path="my-page" element={<MyPageLayout />}>
-          <Route index element={<MyInfo />} />
-          <Route path="applications" element={<AppliedRecruitment />} />
-          <Route path="completed-study" element={<CompletedStudy />} />
-          <Route path="bookmarked/" element={<Bookmark />} />
-          <Route path="bookmarked/:content" element={<Bookmark />} />
-        </Route>
-
-        {/* 404 페이지 */}
-        <Route path="*" element={<NotFound />} />
-
-        {/* 소셜 로그인 */}
-        <Route path="/oauth/kakao" element={<KakaoAuth />} />
-        {/* <Route path="/oauth/naver" element={<NaverAuth />} /> */}
-      </Route>
-
-      {/* 테스트 허브 */}
-      <Route path="/test/*" element={<TestRoutes />} />
-    </Routes>
+        {/* 테스트 허브 */}
+        <Route path="/test/*" element={<TestRoutes />} />
+      </Routes>
+    </Suspense>
   )
 }
 
