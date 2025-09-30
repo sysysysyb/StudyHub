e2d76bbe07b39a412a7a2e83ff823fdc629eba01
JaeHyunLee123 <jhyon123@gmail.com>
2025-09-30 18:03:29 +0900

    ✨ Feature: 탈퇴회원복구 엔드포인트 추가 (#303)

--- src/hooks/api/auth/useUserRecover.ts
+++ src/hooks/api/auth/useUserRecover.ts
@@ -5,20 +5,20 @@ import api from '@/utils/axios'
 import { useMutation, type UseMutationOptions } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 
-export default function useUserRecoverVerify(
+export default function useUserRecover(
   options?: UseMutationOptions<void, Error, UserRecoverVerifyBody>
 ) {
   const { triggerToast } = useToast()
 
   return useMutation({
     mutationFn: async ({ email, verificationCode }) => {
-      await api.post(`${API_BASE_URL}/auth/recover-account/verify`, {
+      await api.post(`${API_BASE_URL}/auth/recover`, {
         email,
         verification_code: verificationCode,
       })
     },
     onSuccess: () => {
-      triggerToast('success', '인증 완료 ✅', '확인 버튼을 눌러주세요.')
+      triggerToast('success', '인증 완료 ✅', '계정 정보가 복구되었습니다.')
     },
     onError: (error) => {
       if (error instanceof AxiosError) {
