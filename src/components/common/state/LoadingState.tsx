4b371ee35d34a2c351ad6ae74b8bf37d3295c17d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-09-30 15:36:33 +0900

    ♻️ Refactor: BaseState에만 사용되던 Spinner 컴포넌트 분리 (#300)

--- src/components/common/state/LoadingState.tsx
+++ src/components/common/state/LoadingState.tsx
@@ -1,9 +1,6 @@
 import type { ComponentProps } from 'react'
-import {
-  BaseStateContent,
-  BaseStateSpinner,
-  BaseStateWrapper,
-} from './BaseState'
+import { BaseStateContent, BaseStateWrapper } from './BaseState'
+import { Spinner } from '@/components'
 
 function LoadingState({
   className,
@@ -11,7 +8,7 @@ function LoadingState({
 }: ComponentProps<typeof BaseStateWrapper>) {
   return (
     <BaseStateWrapper className={className} {...props}>
-      <BaseStateSpinner />
+      <Spinner />
       <BaseStateContent
         title="데이터를 불러오고 있습니다"
         description="잠시만 기다려주세요..."
